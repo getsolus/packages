3f2b3aa90ed319d11267cbea63762958fa811c53
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:37:30 +0100

    xorg-driver-input-wacom: Initial commit

---
+++ packages/x/xorg-driver-input-wacom/actions.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/python
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog")
