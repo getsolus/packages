2fe527eb457ef3d095714e6298278f1e2cad7916
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:27:40 +0100

    perl-archive-zip: Initial commit

---
+++ packages/p/perl-archive-zip/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import perlmodules, pisitools
+
+
+def setup():
+    perlmodules.configure()
+
+
+def build():
+    perlmodules.make()
+
+
+def install():
+    perlmodules.install()
