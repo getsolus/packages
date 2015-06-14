1347fb1bee83dc8a26e836fd73cf730e578494e0
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:29:20 +0100

    libstartup-notification: Initial commit

---
+++ packages/l/libstartup-notification/actions.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "doc/startup-notification.txt")
