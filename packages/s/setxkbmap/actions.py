f5a8fae7be38863d3d7662b0585b4b932250c9c8
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:35:47 +0100

    setxkbmap: Initial commit

---
+++ packages/s/setxkbmap/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
