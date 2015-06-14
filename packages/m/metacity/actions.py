74df11e7a554cc7dad81c189f856471513332d2a
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:31:05 +0100

    metacity: Initial commit

---
+++ packages/m/metacity/actions.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure ("--disable-static")
+
+def build():
+    autotools.make ()
+
+def install():
+    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc ("AUTHORS", "ChangeLog", "COPYING")
