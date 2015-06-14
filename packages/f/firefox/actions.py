5690166793f2605dd46cf1715c848c6e923b4d29
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:41:24 +0100

    firefox: Initial commit

---
+++ packages/f/firefox/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+def setup():
+    pisitools.dosed("mozconfig", "##JOBCOUNT##", get.makeJOBS())
+
+def build():
+    autotools.make("-f client.mk")
+
+
+def install():
+    autotools.rawInstall("-f client.mk install INSTALL_SDK= DESTDIR=%s" % get.installDIR())
+    pisitools.dosym("/usr/lib/firefox-38.0.6/browser/icons/mozicon128.png",
+                     "/usr/share/pixmaps/firefox.png")
+    pisitools.dodir("/usr/lib/mozilla/plugins")
+    pisitools.dosym("/usr/lib/mozilla/plugins",
+                    "/usr/lib/firefox-38.0.6/plugins")
+
