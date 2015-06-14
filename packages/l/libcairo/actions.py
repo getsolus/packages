fd6ba373b6fe79a492016ffc9a93aec596304375
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:29:23 +0100

    libcairo: Initial commit

---
+++ packages/l/libcairo/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    # We need to --enable-cogl in the future for cogl surface support
+    # Disable xlib-xcb - its evil in cairo.
+    autotools.configure("--disable-static \
+                         --disable-xlib-xcb \
+                         --enable-tee \
+                         --enable-gl")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
