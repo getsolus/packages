346920a6edd441fc48fab349d31726d4f40bcdf2
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:29:08 +0100

    libtimezonemap: Initial commit

---
+++ packages/l/libtimezonemap/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+shelltools.export("HOME", get.workDIR())
+
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure()
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
