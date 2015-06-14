fb8a27973a24b62b81a898a8ace2e93a8c736c37
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:40:59 +0100

    libqmi: Initial commit

---
+++ packages/l/libqmi/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("ChangeLog", "COPYING", "AUTHORS")
