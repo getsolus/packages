e9ab68c00694ecc57a8002c21d031e4a0281b03e
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:29:52 +0100

    gvfs: Initial commit

---
+++ packages/g/gvfs/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-gphoto2 \
+                         --disable-documentation \
+                         --enable-afc \
+                         --libexecdir=/usr/lib/gvfs")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
