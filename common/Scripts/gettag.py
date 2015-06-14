79e8f9e3d877affd76e76a3af1c60956c3a20c45
Ikey Doherty <ikey@solus-project.com>
2015-06-14 17:56:42 +0100

    common: gettag: Add missing GPL header

--- common/Scripts/gettag.py
+++ common/Scripts/gettag.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+#  gettag.py: Get git tag for package
+#
+#  Copyright 2015 Ikey Doherty <iikey@solus-project.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+
 import os
 import sys
 
