0e6d4da1a45a7b33f6af18f9c7e4480aa4ebc4f6
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:30:50 +0100

    gnome-maps: Initial commit

---
+++ packages/g/gnome-maps/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure()
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING")
