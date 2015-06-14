71c89fa8f388536d794656c12393b11f545123df
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:40:25 +0100

    autoconf: Initial commit

---
+++ packages/a/autoconf/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from pisi.actionsapi import pisitools, autotools,get
+
+def setup():
+    autotools.configure("--prefix=/usr")
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=\%s" % get.installDIR())
+
+    pisitools.dodoc("README", "BUGS", "ChangeLog*", "NEWS", "AUTHORS")
