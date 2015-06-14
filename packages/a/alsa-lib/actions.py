86eae7ff38ddebb46e7ca19dfa8620080195abe2
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:34:14 +0100

    alsa-lib: Initial commit

---
+++ packages/a/alsa-lib/actions.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ChangeLog", "COPYING")
+
+    ## TODO: Use doxygen to build doc/
