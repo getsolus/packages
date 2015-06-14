297aed56f007e0a6f864a1b8164708c2fdcc4d19
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:39:10 +0100

    gobject-introspection: Initial commit

---
+++ packages/g/gobject-introspection/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+
+IgnoreAutodep = True
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
