377f4a005669d48484091ddfca00444db0af6758
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:28:06 +0100

    libpth: Initial commit

---
+++ packages/l/libpth/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
