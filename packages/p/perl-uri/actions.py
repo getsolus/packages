353102aaf536132aa34ed67d7ab9587a0dc5f8b8
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:27:34 +0100

    perl-uri: Initial commit

---
+++ packages/p/perl-uri/actions.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, perlmodules, pisitools
+
+
+def setup():
+    perlmodules.configure ()
+
+def build():
+    perlmodules.make ()
+
+def install():
+    perlmodules.install ()
