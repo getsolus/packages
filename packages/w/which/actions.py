2e297cd280463fb8ee7650b6c67c496cb8850aee
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:39:12 +0100

    which: Initial commit

---
+++ packages/w/which/actions.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure ("--prefix=/usr")
+
+def build():
+    autotools.make ()
+
+def install():
+    autotools.install ()
+
+    pisitools.dodoc ("COPYING", "AUTHORS", "NEWS", "README")
