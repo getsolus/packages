af17c0a066963c683ad41e8306852a1a6d08963d
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:38:22 +0100

    xinit: Initial commit

---
+++ packages/x/xinit/actions.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--prefix=/usr\
+                        --disable-static\
+                        --with-xinitdir=/etc/X11/app-defaults")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
