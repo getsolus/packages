4f6ddcff2e5b69b0045f55f24f4be5bab02a8d82
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:35:23 +0100

    faad: Initial commit

---
+++ packages/f/faad/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-shared")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "ChangeLog", "AUTHORS")
+    
+    
