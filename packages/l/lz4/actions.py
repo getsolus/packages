a63ca08ce55e96a6435ede6bf50b7b6820d9ddd8
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:38:27 +0100

    lz4: Initial commit

---
+++ packages/l/lz4/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
