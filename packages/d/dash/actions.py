32f2d97baf04442c62ace8b5f2013ad05e868d9e
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:39:00 +0100

    dash: Initial commit

---
+++ packages/d/dash/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+def setup():
+    autotools.rawConfigure("--bindir=/bin\
+                             --mandir=/usr/share/man")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    # Make dash the default shell on our system
+    pisitools.dosym("/bin/dash", "/bin/sh")
+    pisitools.dosym("/bin/dash", "/usr/bin/dash")
