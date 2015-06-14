9132a6c19f14921a9c04fcade2324969f3e64290
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:29:39 +0100

    libmediaart: Initial commit

---
+++ packages/l/libmediaart/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    
