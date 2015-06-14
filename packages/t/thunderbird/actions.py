4d84d9fe62c7523cfb9988743fb1127cfe95706b
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:41:26 +0100

    thunderbird: Initial commit

---
+++ packages/t/thunderbird/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+def setup():
+    pisitools.dosed("mozconfig", "##JOBCOUNT##", get.makeJOBS())
+
+def build():
+    autotools.make("-f client.mk")
+
+
+def install():
+    autotools.rawInstall("-f client.mk install INSTALL_SDK= DESTDIR=%s" % get.installDIR())
+    pisitools.dosym("/usr/lib/thunderbird-31.7.0/chrome/icons/mozicon128.png",
+                     "/usr/share/pixmaps/thunderbird.png")
+    pisitools.dosym("/usr/lib/mozilla/plugins",
+                    "/usr/lib/thunderbird-31.7.0/plugins")
