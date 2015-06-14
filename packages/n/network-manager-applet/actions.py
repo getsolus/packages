a8d1cb073f093889cb7d1249a8f2b759beb13906
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:30:37 +0100

    network-manager-applet: Initial commit

---
+++ packages/n/network-manager-applet/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.configure("--disable-static \
+                         --libexecdir=/usr/lib/network-manager")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
