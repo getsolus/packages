0236aca6743b1e2a21e1550f21c8ddd8dd0c9449
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:35:03 +0100

    irrlichtmt: Initial commit

---
+++ packages/i/irrlichtmt/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import get, autotools, pisitools, shelltools
+
+srcDir = "source/Irrlicht"
+
+
+def build():
+    shelltools.cd(srcDir)
+    autotools.make("sharedlib NDEBUG=1")
+
+
+def install():
+    pisitools.insinto("/usr/include/irrlicht", "include/*")
+    pisitools.dolib("lib/Linux/libIrrlicht.so.1.8.1")
+    pisitools.dosym("/usr/lib/libIrrlicht.so.1.8.1", "/usr/lib/libIrrlicht.so")
+
+    
