cef48da0489a3cd2ddc2973fd6877f8d50af2614
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:37:58 +0100

    libxres: Initial commit

---
+++ packages/l/libxres/actions.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
