48e06564be8b5b3954402beff2e7c5afcc0ad1b4
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:30:00 +0100

    gnome-session: Initial commit

---
+++ packages/g/gnome-session/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static \
+                         --libexecdir=/usr/lib/gnome-session \
+                         --enable-systemd")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
