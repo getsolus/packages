c44fe9f7822474bf314e944aa8487de57c5147a9
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:27:57 +0100

    granite: Initial commit

---
+++ packages/g/granite/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import cmaketools, pisitools
+
+
+def setup():
+    cmaketools.configure()
+
+
+def build():
+    cmaketools.make()
+
+
+def install():
+    cmaketools.install()
+
+    pisitools.dodoc("AUTHORS", "COPYING")
