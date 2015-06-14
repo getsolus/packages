b6c0ed71c1f901b68a36bbb680ae8e64c3490f32
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:27:35 +0100

    perl-error: Initial commit

---
+++ packages/p/perl-error/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import perlmodules, pisitools
+
+
+def setup():
+    perlmodules.configure()
+
+
+def build():
+    perlmodules.make()
+
+
+def install():
+    perlmodules.install()
+
+    pisitools.dodoc("ChangeLog", "LICENSE")
