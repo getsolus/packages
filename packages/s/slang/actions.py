a75f62c3dd51967f1f178b492a1567164587fd7b
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:26:34 +0100

    slang: Initial commit

---
+++ packages/s/slang/actions.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure ()
+
+def build():
+    autotools.make('-j1')
+
+def install():
+    autotools.install ()
