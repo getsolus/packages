133c5e98dcb7c7476d9dea01c1a1794d7e8fc847
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:30:42 +0100

    gnome-keyring: Initial commit

---
+++ packages/g/gnome-keyring/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.configure("--disable-static \
+                          --with-pam-dir=/lib/security \
+                          --disable-doc")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
