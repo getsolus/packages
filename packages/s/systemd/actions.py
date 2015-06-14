a952cf1c21eb3dcd1e5adb3f49e698f013e873c5
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:39:41 +0100

    systemd: Initial commit

---
+++ packages/s/systemd/actions.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+# Avoid g-ir-scanner FTB/SV
+shelltools.export ("HOME", get.installDIR())
+
+# Circular deps  - woo
+IgnoreAutodep = True
+
+def setup():
+    autotools.configure ("--libexecdir=/usr/lib \
+                          --localstatedir=/var  \
+                          --disable-manpages \
+                          --sysconfdir=/etc \
+                          --with-sysvinit-path=/etc/init.d \
+                          --disable-networkd \
+                          --disable-selinux \
+                          --enable-compat-libs \
+                          --with-pamlibdir=/lib/security")
+
+def build():
+    autotools.make ()
+
+def install():
+    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+
+    # udev compatibility stuff
+    pisitools.dosym ("/usr/bin/udevadm", "/sbin/udevadm")
+    pisitools.dosym ("/usr/lib/systemd/systemd-udevd", "/lib/udev/udevd")
+    pisitools.dosym ("/usr/lib/libudev.so", "/usr/lib/libudev.so.0")
+
+    # Final tweaks ^^
+    pisitools.dosym ("/usr/lib/systemd/systemd", "/bin/systemd")
+    pisitools.dosym ("/usr/lib/systemd/systemd", "/sbin/init")
+
+    # Make the journal directory
+    pisitools.dodir ("/var/log/journal")
+
+    # Install controll symlinks
+    for control_item in ["reboot", "shutdown", "poweroff", "halt"]:
+        pisitools.dosym ("/usr/bin/systemctl", "/sbin/%s" % control_item)
+
+    # Remove unwanted rpm macro
+    pisitools.removeDir ("/usr/lib/rpm")
+
+    # We do not use nsswitch.conf - so don't break our networking.
+    pisitools.remove("/usr/share/factory/etc/nsswitch.conf")
