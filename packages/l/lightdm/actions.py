0a7867f8b06165bf768ef3585a5c8d2654495f82
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:28:45 +0100

    lightdm: Initial commit

---
+++ packages/l/lightdm/actions.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    shelltools.system("sed -i -e 's:getgroups:lightdm_&:' tests/src/libsystem.c")
+
+    autotools.configure("--enable-introspection \
+                         --enable-liblightdm-gobject \
+                         --disable-static \
+                         --libexecdir=/usr/lib/lightdm \
+                         --with-greeter-session=lightdm-gtk-greeter")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodir("/usr/lib/systemd/system/graphical.target.wants")
+    pisitools.dosym("/usr/lib/systemd/system/lightdm.service", "/usr/lib/systemd/system/displaymanager.service")
+    pisitools.dosym("/usr/lib/systemd/system/lightdm.service", "/usr/lib/systemd/system/graphical.target.wants/lightdm.service")
