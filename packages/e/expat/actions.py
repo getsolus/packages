0e088dd419e7a36f10f1e11a4b8255bb4b452b3e
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:39:40 +0100

    expat: Initial commit

---
+++ packages/e/expat/actions.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure ("--prefix=/usr --disable-static")
+
+def build():
+    autotools.make ()
+
+def install():
+    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+
+    # Add the documentation
+    pisitools.dodoc ("doc/*.css", "doc/*.png", "doc/*.html")
