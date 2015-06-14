71201e43965017e816f346a74f708b5e047c8f30
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:41:02 +0100

    dhcpcd: Initial commit

---
+++ packages/d/dhcpcd/actions.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+hookFile = "dhcpcd-hooks/50-dhcpcd-compat"
+
+def setup():
+    # We'll run from /run
+    pisitools.dosed (hookFile, "/var/lib", "/run")
+    autotools.configure ("--libexecdir=/lib/dhcpcd\
+                                              --dbdir=/run\
+                                              --sysconfdir=/etc\
+                                              --with-rootprefix=/")
+
+def build():
+    autotools.make ()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc ("README")
+
+    pisitools.insinto ("/lib/dhcpcd/dhcpcd-hooks/", hookFile)
