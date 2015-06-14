bc9e0df56c6f9d464324aa426b994bbd7cc8d822
Ikey Doherty <ikey@solus-project.com>
2015-06-14 16:29:53 +0100

    libgnome-desktop: Initial commit

---
+++ packages/l/libgnome-desktop/actions.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.configure("--disable-static \
+                         --libexecdir=/usr/lib/GnomeDesktop \
+                         --with-gnome-distributor=\"Solus Project\"")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
