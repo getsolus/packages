04b1a3e0e34333a4a327fdc8971d7afc361ae56b
Ikey Doherty <ikey@solus-project.com>
2015-06-15 04:24:36 +0100

    common: Fix package handling to restrict namespace

--- common/Scripts/clone_repos.sh
+++ common/Scripts/clone_repos.sh
@@ -4,10 +4,10 @@ where=`realpath $(dirname $0)`
 while IFS='' read -r line || [[ -n $line ]]; do
     pname=`basename ${line}`
     if [[ ! -d "$pname" ]]; then
-        git clone "https://git.solus-project.com/$line"
+        git clone "https://git.solus-project.com/packages/$line"
         pushd "$pname"
-        git remote set-url origin "https://git.solus-project.com/$line"
-        git remote set-url --push origin "git@solus-project.com:$line"
+        git remote set-url origin "https://git.solus-project.com/packages/$line"
+        git remote set-url --push origin "git@solus-project.com:packages/$line"
         popd
     fi
 
