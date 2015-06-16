926b1d25e194d8988bad9bc8e96a1e34f4bd0a82
Ikey Doherty <ikey@solus-project.com>
2015-06-16 07:30:25 +0100

    libgtk-2: Update to latest upstream for gtk2 branch, fix dep situation

--- packages/l/libgtk-2/actions.py
+++ packages/l/libgtk-2/actions.py
@@ -2,17 +2,20 @@
 
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
-
+import os
 shelltools.export ("HOME", get.workDIR())
 
 def setup():
+    os.system("sed -i 's#l \(gtk-.*\).sgml#& -o \1#' docs/faq/Makefile.in")
+    os.system("sed -i 's#l \(gtk-.*\).sgml#& -o \1#' docs/tutorial/Makefile.in")
     autotools.configure("--disable-static \
                          --enable-xinerama \
-                         --with-x")
+                         --with-x \
+                         --enable-explicit-deps")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("RUN_QUERY_IMMODULES_TEST=false DESTDIR=%s" % get.installDIR())
     pisitools.rename("/usr/bin/gtk-update-icon-cache", "gtk-update-icon-cache-2.0")
