2af78c61df0e9a4a52eaf292d2e4b82d031c7112
Ikey Doherty <ikey@solus-project.com>
2015-06-18 02:22:11 +0100

    dstat: Convert to ypkg

--- packages/d/dstat/actions.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-
-from pisi.actionsapi import autotools, get, pisitools
-
-def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README", "TODO")
