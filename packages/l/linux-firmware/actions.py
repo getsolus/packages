fd1543a232e01980f71691a901eed92a5d52124f
Ikey Doherty <ikey@solus-project.com>
2015-06-30 07:52:10 +0100

    linux-firmware: Update to 20150513

--- packages/l/linux-firmware/actions.py
+++ packages/l/linux-firmware/actions.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
 
 
-from pisi.actionsapi import shelltools, get, pisitools
+from pisi.actionsapi import shelltools, autotools, get, pisitools
 import os
+import shutil
 
-FirmwareSource = "%s/linux-firmware-20140704" % get.workDIR()
+FirmwareSource = "%s/linux-firmware-%s" % (get.workDIR(), get.srcVERSION())
 
-Blacklisted = ["carl9170fw"]
-
-Purged = ["GPL-3", "Makefile", "WHENCE"]
+Blacklisted = ["carl9170fw", "Makefile", "GPL-3"]
 
 def install():
-    for item in Purged:
-        shelltools.unlink (item)
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     for potential in os.listdir (FirmwareSource):
         # Make sure to backup all licences
         if "LICENSE" in potential or "LICENCE" in potential:
             pisitools.dodoc (potential)
+    for i in Blacklisted:
+        p = os.path.join(get.installDIR(), "lib/firmware", i)
+        if os.path.isfile(p):
+            os.unlink(p)
         else:
-            if not potential in Blacklisted:
-                pisitools.insinto ("/lib/firmware", potential)
+            shutil.rmtree(p)
