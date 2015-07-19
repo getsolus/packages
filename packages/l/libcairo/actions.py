20f6925f240b873597abbfd8f5712e96eaba9f18
Ikey Doherty <ikey@solus-project.com>
2015-07-19 18:26:31 +0100

    libcairo: Various fixes to ensure evince does not crash (bug 42)

--- packages/l/libcairo/actions.py
+++ packages/l/libcairo/actions.py
@@ -7,11 +7,18 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 def setup():
     # We need to --enable-cogl in the future for cogl surface support
     # Disable xlib-xcb - its evil in cairo.
+    autotools.autoreconf("-vfi")
     autotools.configure("--disable-static \
                          --disable-xlib-xcb \
                          --enable-tee \
                          --enable-gl \
                          --enable-svg \
+                         --enable-ps \
+                         --enable-pdf \
+                         --enable-xcb \
+                         --enable-xli \
+                         --enable-fc \
+                         --enable-ft \
                          --disable-lto")
 
 def build():
