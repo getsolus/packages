178f19602b2607851acaab279f09bc3beafc5117
Ikey Doherty <ikey@solus-project.com>
2015-08-21 09:48:28 +0100

    systemd: Enable tmpfile and sysusers handling

---
+++ packages/s/systemd/comar/packageHandler.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import piksemel
+import os
+import os.path
+import shutil
+
+def updateSystemConfig(filepath):
+    parse = piksemel.parse(filepath)
+
+    shouldUser = False
+    shouldTmp = False
+
+    for xmlfile in parse.tags("File"):
+        path = xmlfile.getTagData("Path")
+        if not path.startswith("/"):
+            path = "/%s" % path # Just in case
+        if path.startswith("/usr/lib/tmpfiles.d") or path.startswith("/usr/lib64/tmpfiles.d"):
+            shouldTmp = True
+        if path.startswith("/usr/lib/sysusers.d") or path.startswith("/usr/lib/sysusers.d"):
+            shouldUser = True
+        if shouldUser and shouldTmp:
+            break
+
+    if shouldUser:
+        os.system("/usr/bin/systemd-tmpfiles --create")
+    if shouldTmp:
+        os.system("/usr/bin/systemd-sysusers")
+
+
+def setupPackage(metapath, filepath):
+    updateSystemConfig(filepath)
+
+
+def postCleanupPackage(metapath, filepath):
+    pass
