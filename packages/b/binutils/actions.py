4dd6bb3c139e8ebdaeb527f6effdaa5b1ec2453e
Ikey Doherty <ikey@solus-project.com>
2015-08-21 23:37:52 +0100

    binutils: Update to 2.25.1

--- packages/b/binutils/actions.py
+++ packages/b/binutils/actions.py
@@ -8,14 +8,12 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 BuildDir = "%s/%s" % ( get.workDIR(), "binutils-build")
 
 def setup():
-    # Don't install outdated standards.info
-    shelltools.unlink ("etc/standards.info")
     shelltools.system ("sed -i.bak '/^INFO/s/standards.info //' etc/Makefile.in")
     shelltools.makedirs (BuildDir)
 
     shelltools.cd (BuildDir)
     # Configure
-    shelltools.system ("../binutils-2.23.2/configure \
+    shelltools.system ("../binutils-%s/configure \
         --prefix=/usr \
         --enable-lto \
         --enable-multilib \
@@ -23,7 +21,7 @@ def setup():
         --disable-werror \
         --enable-plugins \
         --target=x86_64-solus-linux \
-        --build=x86_64-solus-linux")
+        --build=x86_64-solus-linux" % get.srcVERSION())
 
 def build():
     shelltools.cd (BuildDir)
@@ -34,4 +32,4 @@ def install():
     autotools.rawInstall ("tooldir=/usr DESTDIR=%s" % get.installDIR())
 
     # Include the libiberty header
-    pisitools.insinto ("/usr/include", "../binutils-2.23.2/include/libiberty.h")
+    pisitools.insinto ("/usr/include", "../binutils-%s/include/libiberty.h" % get.srcVERSION())
