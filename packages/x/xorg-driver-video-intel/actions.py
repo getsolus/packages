ce7df83eddde0cb21fde4e488b90695e92fcf92a
Ikey Doherty <ikey@solus-project.com>
2015-09-20 00:03:37 +0100

    xorg-driver-video-intel: Disable full relro - and update again, unbreaking the driver.
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/x/xorg-driver-video-intel/actions.py
+++ packages/x/xorg-driver-video-intel/actions.py
@@ -6,6 +6,8 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
+    shelltools.export("CFLAGS", get.CFLAGS().replace("-Wl,-z,now", ""))
+    shelltools.export("LDFLAGS", get.LDFLAGS().replace("-Wl,-z,now", ""))
     autotools.configure("--with-default-accel=sna \
                          --enable-uxa \
                          --disable-dri3 ")
