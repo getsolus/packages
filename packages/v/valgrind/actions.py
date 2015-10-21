66b282f07fbfb6f79ddca541a62bd1b05dd1efb8
Ikey Doherty <ikey@solus-project.com>
2015-10-21 05:00:18 +0100

    valgrind: Update to latest upstream
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/v/valgrind/actions.py
+++ packages/v/valgrind/actions.py
@@ -6,6 +6,9 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
+    # stack protector = boom, also with relro, fexceptions link issue
+    shelltools.export("CFLAGS", "%s -fno-stack-protector" % get.CFLAGS().replace("-Wl,-z,now", "").replace("-fexceptions",""))
+    shelltools.export("CXXFLAGS", "%s -fno-stack-protector" % get.CXXFLAGS().replace("-Wl,-z,now", "").replace("-fexceptions",""))
     autotools.configure()
 
 def build():
