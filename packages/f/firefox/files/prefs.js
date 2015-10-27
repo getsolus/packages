e0dfe8d76f9e27016e645f5ae9286d0a56cc8c27
Ikey Doherty <ikey@solus-project.com>
2015-10-27 01:59:11 +0000

    firefox: Install default preferences and compat link for theming,etc
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ packages/f/firefox/files/prefs.js
@@ -0,0 +1,16 @@
+pref("general.smoothScroll",                true);
+pref("intl.locale.matchOS",                 true);
+pref("toolkit.networkmanager.disable",      false);
+pref("toolkit.storage.synchronous",         0);
+pref("offline.autoDetect",                  true);
+pref("browser.backspace_action",            0);
+pref("browser.link.open_external",          3);
+pref("browser.shell.checkDefaultBrowser",   false);
+pref("network.manage-offline-status",       true);
+pref("extensions.shownSelectionUI",         true);
+pref("startup.homepage_override_url",       "");
+pref("browser.startup.homepage",            "data:text/plain,browser.startup.homepage=https://solus-project.com/");
+pref("browser.newtabpage.pinned",           '[{"url":"https://solus-project.com/","title":"Solus Project"}]');
+pref("extensions.autoDisableScopes",        0);
+pref("general.skins.selectedSkin",          "arc-darker-theme");
+pref("extensions.enabledAddons",            "%7B8a01dfcc-450f-11e5-909b-fb581d5d46b0%7D:41.20150921");
