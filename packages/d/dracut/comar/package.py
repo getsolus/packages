0688d2b23c8b8fac0f4a16ccd795135e097e1f66
Ikey Doherty <ikey@solus-project.com>
2015-10-31 20:24:07 +0000

    dracut: Garbage collect old initramfs files
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/d/dracut/comar/package.py
+++ packages/d/dracut/comar/package.py
@@ -3,7 +3,49 @@ import os
 import os.path
 import shutil
 
+import glob
+import pisi.version
+
+MAX_PERMITTED = 2
+
+class initramfs:
+
+    path = None
+
+    def __init__(self, path):
+        self.path = path
+        spl = path.split("/boot/initramfs-")[1]
+        self.version = pisi.version.Version(spl.split(".img")[0])
+
+    def __repr__(self):
+        return '%s - %s' % (self.path, self.version)
+
+    def getVersion(self):
+        return self.version
+
+def cleanupOldImages():
+    images = glob.glob("/boot/initramfs-*.img")
+    objs = list()
+    for image in images:
+        try:
+            i = initramfs(image)
+            objs.append(i)
+        except Exception, e:
+            continue
+        
+    simages = sorted(objs, key=initramfs.getVersion, reverse=True)
+
+    if len(simages) > MAX_PERMITTED:
+        death = simages[MAX_PERMITTED:]
+        for kill in death:
+            try:
+                os.unlink(kill.path)
+            except Exception, ex:
+                print "Unable to remove stale initramfs: %s" % ex
+
+
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
+    cleanupOldImages()
     # Generate an initramfs for all installed kernels
     for possible in os.listdir ("/boot"):
         if possible.startswith ("kernel-") or possible.startswith ("vmlinuz-"):
--- packages/d/dracut/comar/packageHandler.py
+++ packages/d/dracut/comar/packageHandler.py
@@ -4,6 +4,47 @@ import os
 import os.path
 import shutil
 
+import glob
+import pisi.version
+
+MAX_PERMITTED = 2
+
+class initramfs:
+
+    path = None
+
+    def __init__(self, path):
+        self.path = path
+        spl = path.split("/boot/initramfs-")[1]
+        self.version = pisi.version.Version(spl.split(".img")[0])
+
+    def __repr__(self):
+        return '%s - %s' % (self.path, self.version)
+
+    def getVersion(self):
+        return self.version
+
+def cleanupOldImages():
+    images = glob.glob("/boot/initramfs-*.img")
+    objs = list()
+    for image in images:
+        try:
+            i = initramfs(image)
+            objs.append(i)
+        except Exception, e:
+            continue
+        
+    simages = sorted(objs, key=initramfs.getVersion, reverse=True)
+
+    if len(simages) > MAX_PERMITTED:
+        death = simages[MAX_PERMITTED:]
+        for kill in death:
+            try:
+                os.unlink(kill.path)
+            except Exception, ex:
+                print "Unable to remove stale initramfs: %s" % ex
+
+
 def updateInitrd(filepath):
     parse = piksemel.parse(filepath)
     for xmlfile in parse.tags("File"):
@@ -31,6 +72,7 @@ def updateInitrd(filepath):
 
 
 def setupPackage(metapath, filepath):
+    cleanupOldImages()
     updateInitrd(filepath)
 
 
