c5c111140a997ee4443c41cd3c60c0a564c18d37
Ikey Doherty <ikey@solus-project.com>
2015-10-31 15:44:37 +0000

    nvidia-glx-driver: Rebuild against 4.1.12 kernel
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/n/nvidia-glx-driver/actions.py
+++ packages/n/nvidia-glx-driver/actions.py
@@ -9,7 +9,7 @@ import commands
 wdir = "NVIDIA-Linux-x86_64-%s" % get.srcVERSION()
 
 # Required... built in tandem with kernel update
-kversion = "4.1.11"
+kversion = "4.1.12"
 
 def setup():
     shelltools.system("sh NVIDIA-Linux-x86_64-%s.run --extract-only" % get.srcVERSION())
--- packages/n/nvidia-glx-driver/comar/package.py
+++ packages/n/nvidia-glx-driver/comar/package.py
@@ -2,7 +2,7 @@
 import os
 import os.path
 
-kver = "4.1.11"
+kver = "4.1.12"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     try:
