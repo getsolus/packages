3a4f869275f96d58af28d439598af27b83452ee4
Ikey Doherty <ikey@solus-project.com>
2015-11-08 19:20:06 +0000

    gdm: Update to 3.18.0
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/g/gdm/comar/package.py
+++ packages/g/gdm/comar/package.py
@@ -11,11 +11,13 @@ def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     try:
         os.system ("groupadd -g %d %s" % (OUR_ID, OUR_NAME))
         os.system ("useradd -m -d /var/lib/gdm -r -s /bin/false -u %d -g %d %s -c %s" % (OUR_ID, OUR_ID, OUR_NAME, OUR_DESC))
+        os.system ("dconf update")
     except:
         pass
 
 def postRemove():
     try:
+        os.system ("dconf update")
         os.system ("userdel %s" % OUR_NAME)
         os.system ("groupdel %s" % OUR_NAME)
     except:
