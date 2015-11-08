c8fb9e8aab412999a028cc94be2b5ff2de695d09
Ikey Doherty <ikey@solus-project.com>
2015-11-08 17:29:33 +0000

    libwebkit-gtk: Disable full relro and LD_AS_NEEDED
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/l/libwebkit-gtk/actions.py
+++ packages/l/libwebkit-gtk/actions.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools, cmaketools
+import os
 
 shelltools.export("HOME", get.workDIR())
 
 def setup():
+    del os.environ["LD_AS_NEEDED"]
+    shelltools.export("CFLAGS", get.CFLAGS().replace("-Wl,-z,now", ""))
+    shelltools.export("CXXFLAGS", get.CXXFLAGS().replace("-Wl,-z,now", ""))
+    shelltools.export("LDFLAGS", get.LDFLAGS().replace("-Wl,-z,now", ""))
     cmaketools.configure("-DPORT=GTK -DCMAKE_SKIP_RPATH=ON")
 
 def build():
