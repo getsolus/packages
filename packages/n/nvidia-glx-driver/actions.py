ebf151ef964bd186f0f83fe7d73e9425ec622bfe
Justin Zobel <justin@solus-project.com>
2015-12-10 12:39:06 +1030

    nvidia-glx-driver: Bump for kernel

--- packages/n/nvidia-glx-driver/actions.py
+++ packages/n/nvidia-glx-driver/actions.py
@@ -9,7 +9,7 @@ import commands
 wdir = "NVIDIA-Linux-x86_64-%s" % get.srcVERSION()
 
 # Required... built in tandem with kernel update
-kversion = "4.1.13"
+kversion = "4.1.14"
 
 def setup():
     shelltools.system("sh NVIDIA-Linux-x86_64-%s.run --extract-only" % get.srcVERSION())
--- packages/n/nvidia-glx-driver/comar/package.py
+++ packages/n/nvidia-glx-driver/comar/package.py
@@ -2,7 +2,7 @@
 import os
 import os.path
 
-kver = "4.1.13"
+kver = "4.1.14"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     try:
