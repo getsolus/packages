85971fc768763271000fce2fa4de1ba79ec2c8d7
Ikey Doherty <ikey@solus-project.com>
2015-12-14 19:52:40 +0000

    nvidia-glx-driver: Enable multilib
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/n/nvidia-glx-driver/actions.py
+++ packages/n/nvidia-glx-driver/actions.py
@@ -20,9 +20,9 @@ def build():
     shelltools.cd("uvm")
     autotools.make("\"SYSSRC=/lib64/modules/%s/build\" module" % kversion)
 
-def link_install(lib, libdir='/usr/lib', abi='1'):
+def link_install(lib, libdir='/usr/lib', abi='1', cdir='.'):
     ''' Install a library with necessary links '''
-    pisitools.dolib("%s.so.%s" % (lib, get.srcVERSION()), libdir)
+    pisitools.dolib("%s/%s.so.%s" % (cdir, lib, get.srcVERSION()), libdir)
     pisitools.dosym("%s/%s.so.%s" % (libdir, lib, get.srcVERSION()), "%s/%s.so.%s" % (libdir, os.path.basename(lib), abi))
     pisitools.dosym("%s/%s.so.%s" % (libdir, lib, abi), "%s/%s.so" % (libdir, os.path.basename(lib)))
         
@@ -38,18 +38,29 @@ def install():
     for lib in libs:
         abi = '2' if lib == "libGLESv2" else "1"
         link_install(lib, "/usr/lib/glx-provider/nvidia", abi)
+        if lib != "libglx":
+            link_install(lib, "/usr/lib32/glx-provider/nvidia", abi, cdir='32')
 
     # non-conflict libraries
     libs =  ["libnvidia-glcore", "libnvidia-eglcore", "libnvidia-glsi",
-        "libnvidia-ifr", "libnvidia-fbc", "libnvidia-encode", "libnvidia-cfg",
-        "libnvidia-ml", "tls/libnvidia-tls", "libcuda", "libnvcuvid",
-        "libnvidia-gtk2", "libnvidia-gtk3"]
+        "libnvidia-ifr", "libnvidia-fbc", "libnvidia-encode",
+        "libnvidia-ml", "libcuda", "libnvcuvid", "libnvidia-opencl"]
+
+    native_libs = ["libnvidia-cfg", "libnvidia-gtk2", "libnvidia-gtk3"]
 
     for lib in libs:
+        link_install(lib)
+        link_install(lib, libdir='/usr/lib32', cdir='32')
+    for lib in native_libs:
         link_install(lib)
 
     # vdpau provider
     link_install("libvdpau_nvidia", "/usr/lib/vdpau")
+    link_install("libvdpau_nvidia", "/usr/lib32/vdpau", cdir='32')
+
+    # TLS
+    link_install("tls/libnvidia-tls")
+    link_install("tls/libnvidia-tls", libdir='/usr/lib32', cdir='32')
 
     # binaries
     bins = ["nvidia-debugdump", "nvidia-xconfig", "nvidia-settings",
---
+++ packages/n/nvidia-glx-driver/comar/multilib.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+import os.path
+
+kver = "4.1.14"
+
+def postInstall(fromVersion, fromRelease, toVersion, toRelease):
+    try:
+        os.system("/usr/bin/gl-driver-switch set-link nvidia")
+    except Exception, e:
+        print "Post-install error: %s" % e
+
+def postRemove():
+    try:
+        os.system("/usr/bin/gl-driver-switch set-link default")
+    except Exception, e:
+        print "Post-remove error: %s" % e
