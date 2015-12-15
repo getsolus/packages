ec021c6be84af95e6e3218304f3f2845e83bf6f6
Ikey Doherty <ikey@solus-project.com>
2015-12-15 02:12:08 +0000

    elfutils: I hate you.
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/e/elfutils/actions.py
+++ packages/e/elfutils/actions.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
-from pisi.actionsapi import get, autotools
+from pisi.actionsapi import get, autotools, pisitools
 
 
 def setup():
+    libdir = "lib64" if get.buildTYPE() != "emul32" else "lib32"
     autotools.configure("--program-prefix=eu \
-                         --disable-static")
+                         --libdir=/usr/%s \
+                         --disable-static" % libdir)
 
 
 def build():
@@ -14,3 +16,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/usr/lib32/elfutils")
