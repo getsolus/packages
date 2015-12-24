29d53a56a8c2c3360f45784b1632ab7f320988e3
Ikey Doherty <ikey@solus-project.com>
2015-12-24 21:26:35 +0000

    gnome-common: Fix conflict with autoconf-archive, depend on it
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/g/gnome-common/actions.py
+++ packages/g/gnome-common/actions.py
@@ -2,7 +2,7 @@
 
 
 from pisi.actionsapi import get, autotools, pisitools
-
+import os
 
 def setup():
     autotools.configure()
@@ -15,4 +15,6 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
+    for i in ["ax_check_enable_debug.m4", "ax_code_coverage.m4"]:
+        os.unlink("%s/usr/share/aclocal/%s" % (get.installDIR(), i))
     pisitools.dodoc("ChangeLog", "COPYING")
