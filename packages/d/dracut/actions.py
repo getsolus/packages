bb6388edc844aa01a3bcc29c51add5dd871ac61d
Ikey Doherty <ikey@solus-project.com>
2016-01-13 04:28:25 +0000

    dracut: Add some default config
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/d/dracut/actions.py
+++ packages/d/dracut/actions.py
@@ -14,3 +14,5 @@ def build():
 
 def install():
     autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    pisitools.removeDir("/usr/lib/dracut/modules.d/00dash")
+    pisitools.removeDir("/usr/lib/dracut/modules.d/00bootchart")
