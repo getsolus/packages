499108d43052fac7d7876c869c6dd3de98efa2e9
Ikey Doherty <ikey@solus-project.com>
2016-01-13 04:32:51 +0000

    dracut: More fixes..
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/d/dracut/actions.py
+++ packages/d/dracut/actions.py
@@ -16,3 +16,4 @@ def install():
     autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
     pisitools.removeDir("/usr/lib/dracut/modules.d/00dash")
     pisitools.removeDir("/usr/lib/dracut/modules.d/00bootchart")
+    pisitools.removeDir("/usr/lib/dracut/modules.d/02caps")
