ee4301c68c0c2e2dc1f214fb27892cf78d5265f7
Ikey Doherty <ikey@solus-project.com>
2016-02-01 21:11:18 +0000

    python-geoip: Enable python 3.4 extension
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/py/python-geoip/actions.py
+++ packages/py/python-geoip/actions.py
@@ -6,7 +6,9 @@ from pisi.actionsapi import pythonmodules
 
 def build():
     pythonmodules.compile()
+    pythonmodules.compile(pyVer='3.4')
 
 
 def install():
     pythonmodules.install()
+    pythonmodules.install(pyVer='3.4')
