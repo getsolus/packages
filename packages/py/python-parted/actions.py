8041684ea14d411583cc1dd4ae9efeb78eba3b39
Ikey Doherty <ikey@solus-project.com>
2016-02-01 21:13:23 +0000

    python-parted: Enable Python 3.4
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/py/python-parted/actions.py
+++ packages/py/python-parted/actions.py
@@ -6,9 +6,11 @@ from pisi.actionsapi import pythonmodules, pisitools
 
 def build():
     pythonmodules.compile()
+    pythonmodules.compile(pyVer='3.4')
 
 
 def install():
     pythonmodules.install()
+    pythonmodules.install(pyVer='3.4')
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "BUGS", "COPYING")
