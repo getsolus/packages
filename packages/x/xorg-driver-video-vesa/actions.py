a9c3e14a216b556181dc83fea683ba0271a9230d
Ikey Doherty <ikey@solus-project.com>
2016-02-25 01:45:31 +0000

    xorg-driver-video-vesa: Rebuild against new xorg
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/x/xorg-driver-video-vesa/actions.py
+++ packages/x/xorg-driver-video-vesa/actions.py
@@ -6,6 +6,8 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
+    shelltools.export("CFLAGS", get.CFLAGS().replace("-Wl,-z,now", ""))
+    shelltools.export("LDFLAGS", get.LDFLAGS().replace("-Wl,-z,now", ""))
     autotools.configure("--prefix=/usr\
                          --disable-static")
 
