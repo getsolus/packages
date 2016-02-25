7517be8df1b753f334ddd317e9bb40919a1b847c
Ikey Doherty <ikey@solus-project.com>
2016-02-25 00:26:00 +0000

    nettle: Enable 32bit libs
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/n/nettle/actions.py
+++ packages/n/nettle/actions.py
@@ -6,7 +6,12 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure("--libdir=/usr/lib")
+    libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib64"
+
+    autotools.configure("--libdir=%s --enable-shared" % libdir)
+
+# Program terminated.
+# ActionsAPI [chmod]: No file matched pattern "/var/eopkg/nettle-2.7.1-8/install/usr/lib64/libhogweed.so.2.5".
 
 def build():
     autotools.make()
@@ -14,7 +19,9 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    for target in ["/usr/lib/libhogweed.so.2.5", "/usr/lib/libnettle.so.4.7"]:
-        shelltools.chmod("%s/%s" % (get.installDIR(), target), mode=0755)
+    libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib64"
+
+    for target in ["%s/libhogweed.so.2.5" % libdir, "%s/libnettle.so.4.7" % libdir]:
+        shelltools.chmod("%s%s" % (get.installDIR(), target), mode=0755)
 
     pisitools.dodoc("nettle.html")
