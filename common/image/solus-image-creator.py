ccaa7ef446e1c83992873461d5d8901c5c1fa3db
Ikey Doherty <ikey@solus-project.com>
2016-02-29 01:19:43 +0000

    common: image: Pass quiet/splash to cmdline
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- common/image/solus-image-creator.py
+++ common/image/solus-image-creator.py
@@ -500,7 +500,7 @@ def create_efi(title, name, label):
             conf.write("""title %(TITLE)s
 linux /kernel
 initrd /initrd
-options root=live:CDLABEL=%(DESC)s ro rd.luks=0 rd.md=0""" % { 'DESC' : label, 'TITLE': title})
+options root=live:CDLABEL=%(DESC)s ro rd.luks=0 rd.md=0 quiet splash""" % { 'DESC' : label, 'TITLE': title})
     except Exception, ex:
         print("Unable to write goofiboot config: %s" % ex)
         clean_exit(1)
@@ -576,7 +576,7 @@ MENU TABMSGROW 11
 label live
   menu label Start %(NAME)s
   kernel /boot/kernel
-  append initrd=/boot/initrd.img root=live:CDLABEL=%(LABEL)s ro rd.luks=0 rd.md=0 --
+  append initrd=/boot/initrd.img root=live:CDLABEL=%(LABEL)s ro rd.luks=0 rd.md=0 quiet splash --
 menu default
 label local
   menu label Boot from local drive
