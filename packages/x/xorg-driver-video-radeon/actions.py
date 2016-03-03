03d0ea488d205d68fddf4bd9e2bda52df6f45392
Ikey Doherty <ikey@solus-project.com>
2016-03-03 06:28:15 +0000

    xorg-driver-video-radeon: Disable relro
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/x/xorg-driver-video-radeon/actions.py
+++ packages/x/xorg-driver-video-radeon/actions.py
@@ -4,6 +4,8 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
+    shelltools.export("CFLAGS", get.CFLAGS().replace("-Wl,-z,now", ""))
+    shelltools.export("LDFLAGS", get.LDFLAGS().replace("-Wl,-z,now", ""))
     autotools.configure()
 
 def build():
