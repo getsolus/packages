9afa470ed104a97f2e05f2b75c9206fe025e5537
Ikey Doherty <ikey@solus-project.com>
2016-03-06 06:21:19 +0000

    nettle: Update to v3.2
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/n/nettle/actions.py
+++ packages/n/nettle/actions.py
@@ -9,10 +9,6 @@ def setup():
     libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib64"
 
     autotools.configure("--libdir=%s --enable-shared" % libdir)
-
-# Program terminated.
-# ActionsAPI [chmod]: No file matched pattern "/var/eopkg/nettle-2.7.1-8/install/usr/lib64/libhogweed.so.2.5".
-
 def build():
     autotools.make()
 
@@ -21,7 +17,7 @@ def install():
 
     libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib64"
 
-    for target in ["%s/libhogweed.so.2.5" % libdir, "%s/libnettle.so.4.7" % libdir]:
+    for target in ["%s/libhogweed.so.4.2" % libdir, "%s/libnettle.so.6.2" % libdir]:
         shelltools.chmod("%s%s" % (get.installDIR(), target), mode=0755)
 
     pisitools.dodoc("nettle.html")
