fce230b510f5c6fc6fa055342003e2e3692b9953
Ikey Doherty <ikey@solus-project.com>
2016-04-13 03:31:35 +0100

    plymouth: Update branding in plymouth (finally)
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/p/plymouth/actions.py
+++ packages/p/plymouth/actions.py
@@ -8,11 +8,15 @@ def setup():
     autotools.configure("--enable-pango \
                          --enable-gtk \
                          --enable-drm \
+                         --enable-drm-renderer \
                          --enable-systemd-integration \
                          --enable-gdm-transition \
                          --enable-pango \
                          --libdir=/usr/lib \
                          --without-system-root-install \
+                         --with-release-file=/etc/os-release \
+                         --with-background-start-color-stop=0x404552 \
+                         --with-background-end-color-stop=0x404552 \
                          --with-logo=/usr/share/pixmaps/Solus.png")
 
 
