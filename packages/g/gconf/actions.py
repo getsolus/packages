7e0faf5498807537082b1fbbb44d87f38e840833
Ikey Doherty <ikey@solus-project.com>
2016-05-22 18:25:35 +0100

    gconf: Enable multilib
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/g/gconf/actions.py
+++ packages/g/gconf/actions.py
@@ -3,22 +3,41 @@
 
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
+import os
+import shutil
 
 shelltools.export("HOME", get.workDIR())
 
 def setup():
-    autotools.configure("--prefix=/usr \
-                         --sysconfdir=/etc \
-                         --libexecdir=/usr/lib/GConf \
+    if get.buildTYPE() == "emul32":
+        libdir = "/usr/lib32"
+        exOpts = "--libdir=/usr/lib32 --disable-defaults-service"
+    else:
+        libdir = "/usr/lib64"
+        exOpts = "--libdir=/usr/lib64"
+
+    autotools.configure("--sysconfdir=/etc \
+                         --libexecdir=%s/GConf \
                          --disable-orbit \
-                         --disable-static")
+                         --disable-static %s" % (libdir, exOpts))
 
 def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    idir = get.installDIR()
+    # Force to subdirectory so we dont trash 64-bit installs
+    if get.buildTYPE() == "emul32":
+        idir += "/derpmcderp"
 
+    autotools.rawInstall("DESTDIR=%s" % idir)
+
+    if get.buildTYPE() == "emul32":
+        pisitools.dodir("/usr")
+        shelltools.system("mv \"%s/usr/lib32\" \"%s/usr/.\"" % (idir, get.installDIR()))
+        shutil.rmtree(idir)
+        return
+    
     pisitools.dosym("/etc/gconf/gconf.xml.defaults", "/etc/gconf/gconf.xml.system")
 
     pisitools.dodoc("ChangeLog", "COPYING")
