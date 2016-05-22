e116532db5c026d55e9889829759ffeed62557a6
Ikey Doherty <ikey@solus-project.com>
2016-05-22 02:09:23 +0100

    pango: Enable multilib
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/p/pango/actions.py
+++ packages/p/pango/actions.py
@@ -7,7 +7,11 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 def setup():
     shelltools.export ("HOME", get.workDIR())
-    autotools.configure("--disable-static --enable-shared")
+    libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib64"
+    bindir = "/usr/lib32/pango/bin" if get.buildTYPE() == "emul32" else "/usr/bin"
+    prefix = "/emul32" if get.buildTYPE() == "emul32" else "/usr"
+
+    autotools.configure("--disable-static --enable-shared --prefix=%s --libdir=%s --bindir=%s" % (prefix, libdir, bindir))
 
 def build():
     shelltools.export ("HOME", get.workDIR())
