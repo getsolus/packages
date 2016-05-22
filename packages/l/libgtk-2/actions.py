001bf1f7ce8932b393142e59872e55335136b3e7
Ikey Doherty <ikey@solus-project.com>
2016-05-22 02:36:32 +0100

    libgtk-2: Enable multilib without cups for now
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/l/libgtk-2/actions.py
+++ packages/l/libgtk-2/actions.py
@@ -9,14 +9,20 @@ def setup():
     os.system("sed -i 's#l \(gtk-.*\).sgml#& -o \1#' docs/faq/Makefile.in")
     os.system("sed -i 's#l \(gtk-.*\).sgml#& -o \1#' docs/tutorial/Makefile.in")
     autotools.autoreconf()
+    libdir = "/usr/lib32" if get.buildTYPE() == "emul32" else "/usr/lib64"
+
+    # Temporary, until we get emul32 cups
+    confopts = "--disable-cups" if get.buildTYPE() == "emul32" else ""
     autotools.configure("--disable-static \
+                         --libdir=%s \
                          --enable-xinerama \
                          --with-x \
-                         --enable-explicit-deps")
+                         --enable-explicit-deps %s" % (libdir, confopts))
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("RUN_QUERY_IMMODULES_TEST=false DESTDIR=%s" % get.installDIR())
-    pisitools.rename("/usr/bin/gtk-update-icon-cache", "gtk-update-icon-cache-2.0")
+    if get.buildTYPE() != "emul32":
+        pisitools.rename("/usr/bin/gtk-update-icon-cache", "gtk-update-icon-cache-2.0")
