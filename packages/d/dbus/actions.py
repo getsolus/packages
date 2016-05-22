38f43584f4a8c5067dc20e5ab3d2a6b2ab51a1c3
Ikey Doherty <ikey@solus-project.com>
2016-05-22 06:07:04 +0100

    dbus: Enable multilib
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/d/dbus/actions.py
+++ packages/d/dbus/actions.py
@@ -2,20 +2,35 @@
 
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
+import os
+import shutil
 
 IgnoreAutodep = True
 
 def setup():
+    libdir = "/usr/lib" if get.buildTYPE() != "emul32" else "/usr/lib32"
+    confEx = "--disable-tests" if get.buildTYPE() == "emul32" else ""
     autotools.configure ("--prefix=/usr \
                           --sysconfdir=/etc \
                           --localstatedir=/var \
                           --libexecdir=/usr/lib/dbus-1.0 \
                           --with-console-auth-dir=/run/console/\
                           --with-systemdsystemunitdir=/usr/lib/systemd/system\
-                          --disable-static")
+                          --libdir=%s \
+                          --disable-static %s" % (libdir, confEx))
 
 def build():
     autotools.make ()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    idir = get.installDIR()
+    if get.buildTYPE() == "emul32":
+        idir += "/derpmcderp"
+
+    autotools.rawInstall ("DESTDIR=%s" % idir)
+    if get.buildTYPE() != "emul32":
+        os.system("chmod o+x %s/usr/lib/dbus-1.0/dbus-daemon-launch-helper" % get.installDIR())
+    else:
+        pisitools.dodir("/usr")
+        shelltools.system("mv \"%s/usr/lib32\" \"%s/usr/.\"" % (idir, get.installDIR()))
+        shutil.rmtree(idir)
