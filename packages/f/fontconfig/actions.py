a3cf198d6b49fa1b3784de0dde80ba380b9fa71f
Ikey Doherty <ikey@solus-project.com>
2016-05-25 22:33:43 +0100

    fontconfig: Fix prefix for fontconfig
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/f/fontconfig/actions.py
+++ packages/f/fontconfig/actions.py
@@ -3,6 +3,8 @@
 
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
+import os
+import shutil
 
 
 def setup():
@@ -10,6 +12,7 @@ def setup():
     autotools.configure ("--disable-static\
                           --disable-docs\
                           --libdir=%s\
+                          --prefix=/usr \
                           --with-baseconfigdir=/usr/share/fonts \
                           --with-configdir=/usr/share/fonts/conf.d \
                           --docdir=/usr/share/doc/fontconfig-2.10.2" % libdir)
@@ -18,4 +21,14 @@ def build():
     autotools.make ()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    idir = get.installDIR()
+    # Force to subdirectory so we dont trash 64-bit installs
+    if get.buildTYPE() == "emul32":
+        idir += "/derpmcderp"
+
+    autotools.rawInstall ("DESTDIR=%s" % idir)
+
+    if get.buildTYPE() == "emul32":
+        pisitools.dodir("/usr")
+        shelltools.system("mv \"%s/usr/lib32\" \"%s/usr/.\"" % (idir, get.installDIR()))
+        shutil.rmtree(idir)
