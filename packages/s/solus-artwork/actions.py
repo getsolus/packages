b3e104278edd638835872d34c7de3ae08db7acda
Ikey Doherty <ikey@solus-project.com>
2016-06-05 14:08:42 +0100

    solus-artwork: Add icons, took time. Much crying
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/s/solus-artwork/actions.py
+++ packages/s/solus-artwork/actions.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 
-from pisi.actionsapi import autotools, pisitools
+from pisi.actionsapi import autotools, pisitools, shelltools, get
 
 def setup():
-    autotools.configure()
+    shelltools.system("autoreconf -vfi")
+    shelltools.system("./configure --prefix=/usr")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    shelltools.system("make install DESTDIR=%s" % get.installDIR())
     pisitools.dosym("/usr/share/themes/Vertex", "/usr/share/themes/Evotex")
     # Compatibility symlink..
     pisitools.dosym("/usr/share/backgrounds/solus", "/usr/share/backgrounds/evolve-os")
