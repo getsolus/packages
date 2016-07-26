25cc84b99b2dcfadd0654bacc593526c93f84652
Ikey Doherty <ikey@solus-project.com>
2016-07-26 03:13:34 +0100

    glib2: Fix derps in multilib with new GNOME
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/g/glib2/actions.py
+++ packages/g/glib2/actions.py
@@ -5,8 +5,9 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 def setup():
     libdir = "lib64" if get.buildTYPE() != "emul32" else "lib32"
+    host = "" if get.buildTYPE() != "emul32" else "--build=i686-pc-linux-gnu --host=i686-pc-linux-gnu"
     autotools.configure("--with-pcre=system \
-                         --libdir=/usr/%s" % libdir)
+                         --libdir=/usr/%s %s" % (libdir, host))
 
 def build():
     autotools.make()
