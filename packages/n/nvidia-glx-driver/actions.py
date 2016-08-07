8ad07fb137041c136caf0c8eef71d3352dd47a6a
Ikey Doherty <ikey@solus-project.com>
2016-08-07 22:49:40 +0100

    nvidia-glx-driver: Port to Linux 4.7
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/n/nvidia-glx-driver/actions.py
+++ packages/n/nvidia-glx-driver/actions.py
@@ -9,10 +9,12 @@ import commands
 wdir = "NVIDIA-Linux-x86_64-%s" % get.srcVERSION()
 
 # Required... built in tandem with kernel update
-kversion = "4.4.16"
+kversion = "4.7.0"
 
 def setup():
     shelltools.system("sh NVIDIA-Linux-x86_64-%s.run --extract-only" % get.srcVERSION())
+    shelltools.cd(wdir)
+    shelltools.system("patch -p1 < ../0001-Port-to-Linux-4.7-API.patch")
 
 def build():
     shelltools.cd(wdir + "/kernel")
--- packages/n/nvidia-glx-driver/comar/package.py
+++ packages/n/nvidia-glx-driver/comar/package.py
@@ -2,7 +2,7 @@
 import os
 import os.path
 
-kver = "4.4.16"
+kver = "4.7.0"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     try:
