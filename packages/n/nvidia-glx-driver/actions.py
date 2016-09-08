b0cf4cff9f9ebf7f26af6241325d37c22c45ab11
Ikey Doherty <ikey@solus-project.com>
2016-09-08 02:00:01 +0100

    nvidia-glx-driver: Rebuild for 4.7.3
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/n/nvidia-glx-driver/actions.py
+++ packages/n/nvidia-glx-driver/actions.py
@@ -9,7 +9,7 @@ import commands
 wdir = "NVIDIA-Linux-x86_64-%s" % get.srcVERSION()
 
 # Required... built in tandem with kernel update
-kversion = "4.7.2"
+kversion = "4.7.3"
 
 def setup():
     shelltools.system("sh NVIDIA-Linux-x86_64-%s.run --extract-only" % get.srcVERSION())
--- packages/n/nvidia-glx-driver/comar/multilib.py
+++ packages/n/nvidia-glx-driver/comar/multilib.py
@@ -2,7 +2,7 @@
 import os
 import os.path
 
-kver = "4.7.2"
+kver = "4.7.3"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     try:
