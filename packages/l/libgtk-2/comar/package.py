4faa20287d802b159588ebf3041948f2a61d587e
Ikey Doherty <ikey@solus-project.com>
2016-11-05 14:04:20 +0000

    libgtk-2: Fix ibus
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/l/libgtk-2/comar/package.py
+++ packages/l/libgtk-2/comar/package.py
@@ -5,5 +5,5 @@ def postInstall(fromVersion, fromRelease, toVersion, toRelease):
 	# Update the known input method modules
 	# Even if we push a package update, this is automatically updated
 	# on the client system
-	os.system ("gtk-query-immodules-2.0 > /etc/gtk-2.0/gtk.immodules")
+	os.system ("gtk-query-immodules-2.0")
 	
