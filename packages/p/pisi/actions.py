b04b63119bf52e66296cd880caeb5ed54007e657
Ikey Doherty <ikey@solus-project.com>
2016-11-10 23:39:09 +0000

    pisi: Complete move to eopkg
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/p/pisi/actions.py
+++ packages/p/pisi/actions.py
@@ -3,9 +3,8 @@ from pisi.actionsapi import get, pisitools, shelltools, pythonmodules
 def install():
     pythonmodules.install()
 
-    pisitools.dosym("pisi-cli", "/usr/bin/pisi")
-    pisitools.dosym("pisi-cli", "/usr/bin/eopkg")
-    pisitools.dodir("/etc/pisi")
-    pisitools.dosym("/etc/eopkg/eopkg.conf", "/etc/pisi/pisi.conf")
+    pisitools.dosym("eopkg-cli", "/usr/bin/eopkg")
+    pisitools.dosym("uneopkg", "/usr/bin/unpisi")
+    pisitools.dosym("lseopkg", "/usr/bin/lspisi")
     pisitools.dodir("/etc/mudur")
     shelltools.echo("%s/etc/mudur/locale" % get.installDIR(), "")
