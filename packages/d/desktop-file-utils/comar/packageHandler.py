c74a58982cbcd9a674fa57e9c02cfb03c48f121d
Ikey Doherty <ikey@solus-project.com>
2016-12-22 16:59:51 +0000

    desktop-file-utils: Update to 0.23 and cleanup the COMAR handler
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/d/desktop-file-utils/comar/packageHandler.py
+++ packages/d/desktop-file-utils/comar/packageHandler.py
@@ -3,16 +3,20 @@
 import piksemel
 import os
 
-def updateDesktopDatabase (filepath):
-	parse = piksemel.parse (filepath)
-	for xmlfile in parse.tags("File"):
-		path = xmlfile.getTagData ("Path")
-		if "/share/applications/" in path:
-			os.system ("/usr/bin/update-desktop-database")
-			break
+def updateDesktopDatabase(filepath):
+    needUpdate = False
+    parse = piksemel.parse(filepath)
+    for xmlfile in parse.tags("File"):
+        path = xmlfile.getTagData("Path")
+        if "/share/applications/" in path:
+            needUpdate = True
+            break
 
-def setupPackage (metapath, filepath):
-    updateDesktopDatabase (filepath)
+    if needUpdate:
+        os.system("/usr/bin/update-desktop-database")
 
-def postCleanupPackage (metapath, filepath):
-    updateDesktopDatabase (filepath)
+def setupPackage(metapath, filepath):
+    updateDesktopDatabase(filepath)
+
+def postCleanupPackage(metapath, filepath):
+    updateDesktopDatabase(filepath)
