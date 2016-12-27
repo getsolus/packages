573ccb46ea17f73c1b4c878e5efc0344e147731d
Bryan T. Meyers <bmeyers@datadrake.com>
2016-12-27 16:47:19 -0500

    xmodmap: That was not the right way to fix that

--- packages/x/xmodmap/files/50-xmodmap.sh
+++
@@ -1,7 +0,0 @@
-# Begin /usr/share/defaults/etc/profile.d/50-xmodmap.sh
-
-if [ -s "$HOME/.Xmodmap" ]; then
-    xmodmap "$HOME/.Xmodmap"
-fi
-
-# End /usr/share/defaults/etc/profile.d/50-xmodmap.sh
