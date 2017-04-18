9df94dc2caa1bd77980949e4d116f39e63860d81
Ikey Doherty <ikey@solus-project.com>
2017-04-18 22:58:50 +0100

    nvidia-glx-driver: Rebuild for 4.9.23-19.lts
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/n/nvidia-glx-driver/actions.py
+++ packages/n/nvidia-glx-driver/actions.py
@@ -9,7 +9,7 @@ import commands
 wdir = "NVIDIA-Linux-x86_64-%s" % get.srcVERSION()
 
 # Required... built in tandem with kernel update
-kversion = "4.9.22-17.lts"
+kversion = "4.9.23-19.lts"
 
 def setup():
     shelltools.system("sh NVIDIA-Linux-x86_64-%s.run --extract-only" % get.srcVERSION())
--- packages/n/nvidia-glx-driver/comar/multilib.py
+++ packages/n/nvidia-glx-driver/comar/multilib.py
@@ -2,7 +2,7 @@
 import os
 import os.path
 
-kver = "4.9.22-17.lts"
+kver = "4.9.23-19.lts"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     try:
