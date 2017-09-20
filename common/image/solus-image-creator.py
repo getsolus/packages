fc6516fe56729f91a919e0675666fb165838c311
Ikey Doherty <ikey@solus-project.com>
2017-09-20 18:08:17 +0100

    common: Kernel got fat, yo
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- common/image/solus-image-creator.py
+++ common/image/solus-image-creator.py
@@ -458,7 +458,7 @@ def create_efi(title, name, label):
             clean_exit(1)
 
     # in future we **must** check kernel/initrd sizes..
-    size = 30
+    size = 34
     cmd = "dd if=/dev/zero of=\"%s\" bs=1 count=0 seek=%sM" % (get_efi_path(), size)
     try:
         ret = check_call(cmd)
