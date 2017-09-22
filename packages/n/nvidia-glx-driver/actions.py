6b96f49f8736a65231bbe2ce0094c84e93923ae3
Ikey Doherty <ikey@solus-project.com>
2017-09-22 04:58:49 +0100

    nvidia-glx-driver: Update to 384.90
    Fix numerous regressions from the short-lived branch
    
    Full changelog [available here](http://www.nvidia.com/Download/driverResults.aspx/123918/en-us)
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/n/nvidia-glx-driver/actions.py
+++ packages/n/nvidia-glx-driver/actions.py
@@ -8,8 +8,8 @@ import commands
 
 # Required... built in tandem with kernel update
 kernel_trees = {
-    "linux-lts": "4.9.51-47.lts",
-    "linux-current": "4.12.14-20.current"
+    "linux-lts": "4.9.51-48.lts",
+    "linux-current": "4.12.14-21.current"
 }
 
 def setup():
