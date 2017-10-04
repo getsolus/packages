3feadfa5fd85297d924fec485ef53938dde2add2
René Vangsgaard <rene.vangsgaard@gmail.com>
2017-10-04 22:13:55 +0100

    sbt: Bump version and remove deprecated JVM argument
    Summary:
    SBT released version 1.0.2. Removed a jvm argument that made the JVM
    print an error message each time the sbt script was run.
    
    Signed-off-by: René Vangsgaard <rene.vangsgaard@gmail.com>
    
    Test Plan:
    Use the seed feature of sbt, which creates a project based on a remote template. Verify that compilation works.
    
    Steps:
      # sbt new scala/scala-seed.g8
      # cd scala-seed-project/
      # sbt run
      # This should be printed to the console
    
    ```
    [info] Loading project definition from /tmp/scala-seed-project/project
    [info] Set current project to Hello (in build file:/tmp/scala-seed-project/)
    [info] Running example.Hello
    hello
    
    ```
    
    In addition, you can verify that the test are run successfully by executing
    
    ```
    sbt test
    ```
    
    Reviewers: #triage_team, joebonrichie
    
    Reviewed By: joebonrichie
    
    Subscribers: joebonrichie
    
    Differential Revision: https://dev.solus-project.com/D1032

--- packages/s/sbt/files/sbt.sh
+++ packages/s/sbt/files/sbt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
+if [[ -z "${SBT_OPTS}" ]]; then
+  SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled"
+fi
 java $SBT_OPTS -jar /usr/share/sbt/sbt-launch.jar "$@"
