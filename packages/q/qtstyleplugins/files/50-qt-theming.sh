c08522f279ef300ec5b57ff138f5f15444e2572b
Peter O'Connor <peter@solus-project.com>
2017-10-26 10:34:35 +1100

    qtstyleplugins: Ban this from Plasma sessions
    Signed-off-by: Peter O'Connor <peter@solus-project.com>

--- packages/q/qtstyleplugins/files/50-qt-theming.sh
+++ packages/q/qtstyleplugins/files/50-qt-theming.sh
@@ -4,4 +4,8 @@ if [ -z "${QT_QPA_PLATFORMTHEME}" ]; then
     export QT_QPA_PLATFORMTHEME="gtk2"
 fi
 
+if [ "${XDG_CURRENT_DESKTOP}" = "KDE" ]; then
+    unset QT_QPA_PLATFORMTHEME
+fi
+
 # End /usr/share/defaults/etc/profile.d/50-qt-theming.sh
