a3e83c1bfca7fc167a730ede02e5397a5dad7faa
Ikey Doherty <ikey@solus-project.com>
2017-11-29 16:37:54 +0000

    libgtk-2: Update GTK2 to 2.24.31 and convert to ypkg (T5010)
    The GTK2 relevant triggers are now housed in usysconf.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- packages/l/libgtk-2/comar/package.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-	# Update the known input method modules
-	# Even if we push a package update, this is automatically updated
-	# on the client system
-	os.system ("gtk-query-immodules-2.0")
-	
