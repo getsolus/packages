e37cad156e99a1ec0de263794efbad744cbe27a8
Ikey Doherty <ikey@solus-project.com>
2017-12-07 15:45:16 +0000

    common: image: Teach spinner how to nuke autoinstall file
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- common/image/solus-image-creator.py
+++ common/image/solus-image-creator.py
@@ -784,6 +784,11 @@ def main():
     # Now ensure we have a clean image
     do_umount(get_cache_target())
     run_chroot("eopkg delete-cache")
+    # Reset auto-installed state for the ISO
+    autofile = os.path.join(get_image_root(), "var/lib/eopkg/info/autoinstalled")
+    if os.path.exists(autofile):
+        print("Removing new autoinstalled state tracker")
+        os.unlink(autofile)
     # Trick systemd into not running units it does not need to...
     run_chroot("/usr/bin/systemd-sysusers")
     run_chroot("touch /etc/.updated -r /etc")
