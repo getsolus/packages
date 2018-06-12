3e362b3f1cf923b1de2554fa0a22ec19a9b2a8a3
Peter O'Connor <peter@solus-project.com>
2018-06-12 17:44:11 +1000

    extra-cmake-modules: Update to 5.47.0
    [Changelog](https://www.kde.org/announcements/kde-frameworks-5.47.0.php)

--- packages/e/extra-cmake-modules/files/qml_cache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in `find -type f -name "*.qml"`; do
-    if ! [ -a "${i}"c ]; then
-        echo "Generating QML cache file ${i}"
-        qmlcachegen --target-architecture=x86_64 --target-abi=x86_64-little_endian-lp64 -o "${i}"c "${i}"
-    fi
-done
