5f87e4b616be37e9292908a3838c7fecedb0eab2
F. von Gellhorn <flinux@vongellhorn.ch>
2018-10-29 17:53:36 +1100

    rust: Update rust to 1.30.0
    Summary:
    Update rust to 1.30.0
    Release notes available here [Link](https://github.com/rust-lang/rust/blob/1.30.0/RELEASES.md)
    
    Test Plan: build bat and firefox successfully against the new rust
    
    Reviewers: #triage_team, sunnyflunk
    
    Reviewed By: #triage_team, sunnyflunk
    
    Differential Revision: https://dev.getsol.us/D4150

--- packages/r/rust/update_sources.sh
+++ packages/r/rust/update_sources.sh
@@ -1,5 +1,5 @@
 # Fetch the latest sources and sha256sums automagically
-VERSION=1.29.2
+VERSION=1.30.0
 mkdir /tmp/rust; cd /tmp/rust
 
 wget https://raw.githubusercontent.com/rust-lang/rust/${VERSION}/src/stage0.txt
