934a7f7301557dde5d468ee7b283a0e417255234
Peter O'Connor <sunnyflunk@gmail.com>
2019-01-15 09:26:59 +1100

    qtstyleplugins: Disable Qt scaling with gtk2 theme so it doesn't scale twice

--- packages/q/qtstyleplugins/files/50-qt-theming.sh
+++ packages/q/qtstyleplugins/files/50-qt-theming.sh
@@ -1,11 +1,9 @@
 # Begin /usr/share/defaults/etc/profile.d/50-qt-theming.sh
 
-if [ -z "${QT_QPA_PLATFORMTHEME}" ]; then
+if [ -z "${QT_QPA_PLATFORMTHEME}" ] && [ "${XDG_CURRENT_DESKTOP}" != "KDE" ]; then
     export QT_QPA_PLATFORMTHEME="gtk2"
-fi
-
-if [ "${XDG_CURRENT_DESKTOP}" = "KDE" ]; then
-    unset QT_QPA_PLATFORMTHEME
+    # Let GTK sort the scaling
+    export QT_AUTO_SCREEN_SCALE_FACTOR=0
 fi
 
 # End /usr/share/defaults/etc/profile.d/50-qt-theming.sh
