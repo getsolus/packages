ecb4461fcac90a75d27e7bc199cd123410782f38
Fabio Forni <fabio@redaril.me>
2019-04-30 18:05:58 +0300

    rust: Update rust to 1.34.0 and extract cargo package
    Summary:
    You can find the release notes on the Rust blog:
    - [1.33](https://blog.rust-lang.org/2019/02/28/Rust-1.33.0.html)
    - [1.34](https://blog.rust-lang.org/2019/04/11/Rust-1.34.0.html)
    
    **Package changes**
    - Enable i686 target
    
    I also want to thank @Girtablulu for the initial assistance.
    
    Test Plan: Successfully built `bat` and `librsvg` with local `rust` and `cargo`.
    
    Reviewers: #triage_team, JoshStrobl
    
    Reviewed By: #triage_team, JoshStrobl
    
    Subscribers: JoshStrobl, Girtablulu
    
    Differential Revision: https://dev.getsol.us/D5642

--- packages/r/rust/update_sources.sh
+++ packages/r/rust/update_sources.sh
@@ -1,11 +1,15 @@
+#!/bin/sh
 # Fetch the latest sources and sha256sums automagically
-VERSION=1.32.0
-mkdir /tmp/rust; cd /tmp/rust
 
-wget https://raw.githubusercontent.com/rust-lang/rust/${VERSION}/src/stage0.txt
-DATE=`cat stage0.txt | grep ^date | awk '{ print $2 }'`
-RUSTV=`cat stage0.txt | grep ^rustc | awk '{ print $2 }'`
-CARGOV=`cat stage0.txt | grep ^cargo | awk '{ print $2 }'`
+VERSION=$1
+
+alias silent_wget='wget -q'
+
+mkdir /tmp/rust; cd /tmp/rust
+silent_wget https://raw.githubusercontent.com/rust-lang/rust/${VERSION}/src/stage0.txt
+DATE=$(grep '^date' stage0.txt | awk '{ print $2 }')
+RUSTV=$(grep '^rustc' stage0.txt | awk '{ print $2 }')
+CARGOV=$(grep '^cargo' stage0.txt | awk '{ print $2 }')
 
 URLS="
 https://static.rust-lang.org/dist/rustc-${VERSION}-src.tar.gz
@@ -15,10 +19,10 @@ https://static.rust-lang.org/dist/${DATE}/cargo-${CARGOV}-x86_64-unknown-linux-g
 "
 
 for i in ${URLS}; do
-    wget ${i}.sha256
+    silent_wget ${i}.sha256
 done
 for i in ${URLS}; do
-    j=`basename ${i}`
-    echo "    - ${i} : `cat ${j}.sha256 | awk '{ print $1 }'`"
+    j=$(basename ${i})
+    echo "    - ${i} : $(awk '{ print $1 }' ${j}.sha256)"
 done
 rm -rf /tmp/rust
