5c7610c737a1e82b23c889d57d8f6d4f1c127e9f
Fabio Forni <fabio@redaril.me>
2019-06-25 12:18:09 +0300

    josm: Initial commit of josm
    Summary: Fixes T5874
    
    Test Plan: Updated two nodes near my home.
    
    Reviewers: #triage_team, DataDrake, JoshStrobl
    
    Reviewed By: #triage_team, JoshStrobl
    
    Subscribers: DataDrake
    
    Maniphest Tasks: T5874
    
    Differential Revision: https://dev.getsol.us/D6090

---
+++ packages/j/josm/create-tarball.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Check the current stable revision from https://josm.openstreetmap.de/wiki/Download
+revision=$1
+src_name=josm-$revision
+
+svn export -r $revision https://josm.openstreetmap.de/svn/trunk /tmp/$src_name
+if [ $? -eq 0 ]; then
+	tar cJf $src_name.tar.xz -C /tmp $src_name
+fi
+rm -rf /tmp/$src_name
