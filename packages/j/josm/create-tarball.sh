4ec404c960e7f24958b0ee5b7d90deeac666cfab
Fabio Forni <fabio@redaril.me>
2020-04-15 02:14:23 -0400

    josm: Update josm to 15553
    Summary:
    - [15553](https://josm.openstreetmap.de/wiki/Changelog#stable-release-19.11)
    - [15492](https://josm.openstreetmap.de/wiki/Changelog#stable-release-19.10)
    
    Test Plan: Downloaded an area and browsed it.
    
    Reviewers: #triage_team, DataDrake
    
    Reviewed By: #triage_team, DataDrake
    
    Subscribers: DataDrake
    
    Differential Revision: https://dev.getsol.us/D7709

--- packages/j/josm/create-tarball.sh
+++ packages/j/josm/create-tarball.sh
@@ -7,5 +7,6 @@ src_name=josm-$revision
 svn export -r $revision https://josm.openstreetmap.de/svn/trunk /tmp/$src_name
 if [ $? -eq 0 ]; then
 	tar cJf $src_name.tar.xz -C /tmp $src_name
+	sha256sum $src_name.tar.xz
 fi
 rm -rf /tmp/$src_name
