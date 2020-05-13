721f6d383645110049d8ff5bc135e4f2eb8930a5
Fabio Forni <fabio@redaril.me>
2020-05-13 02:58:54 -0400

    bisq: Make bisq use openjdk-8 specifically
    Summary: Make bisq use openjdk-8 specifically
    
    Test Plan: It still starts from both the terminal and .desktop file.
    
    Reviewers: #triage_team, DataDrake
    
    Reviewed By: #triage_team, DataDrake
    
    Subscribers: DataDrake
    
    Differential Revision: https://dev.getsol.us/D8831

--- packages/b/bisq/files/bisq.sh
+++ packages/b/bisq/files/bisq.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-exec "/usr/bin/java" -Dsecurity.provider.11=org.bouncycastle.jce.provider.BouncyCastleProvider -jar /usr/share/java/bisq/shaded.jar "$@"
+exec /usr/lib64/openjdk-8/bin/java -Dsecurity.provider.11=org.bouncycastle.jce.provider.BouncyCastleProvider -jar /usr/share/bisq/bisq.jar "$@"
 
