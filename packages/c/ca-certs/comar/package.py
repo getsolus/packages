62e39efcdf1834ae2986ce6162fd5f5d7af77fb2
Fabio Forni <fabio@redaril.me>
2020-05-30 08:29:50 -0400

    ca-certs: Update ca-certs to 20200513 and switch to ypkg format
    Summary:
    Update ca-certs to 20200513 and switch to ypkg format.
    Fixes T5937.
    
    Test Plan:
    System still works.
    No errors returned from `openssl s_client -connect getsol.us:443` and `GET /`.
    `curl` correctly finds certificates.
    
    Reviewers: #triage_team, DataDrake
    
    Reviewed By: #triage_team, DataDrake
    
    Subscribers: DataDrake
    
    Maniphest Tasks: T5937
    
    Differential Revision: https://dev.getsol.us/D7096

--- packages/c/ca-certs/comar/package.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import os
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-	os.system ("c_rehash")
-	
--- packages/c/ca-certs/files/build_certs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-url="http://anduin.linuxfromscratch.org/BLFS/other/certdata.txt"
-
-curl $url -o certdata.txt                &&
-unset url                                &&
-./make-ca.sh                             &&
-./remove-expired-certs.sh certs
