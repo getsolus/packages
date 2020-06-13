c8067128f30add7e02552f7e0d56c7e5897230d1
Campbell Jones <git@serebit.com>
2020-06-13 17:31:07 -0400

    sbt: Update sbt to 1.3.12
    Summary:
    Additionally, switch to using JDK11 for build and runtime.
    
    Notable changes:
    - Updates lm-coursier to 2.0.0-RC6-4, which now uses OS-specific cache locations
    - Updates Apache Ivy to handle HTTP redirects
    - Updates sbt-giter8-resolver to 0.12.0, which brings in giter8.version support in project/build.properties
    - Displays welcome banner with sbt version and Java version
    
    Test Plan: Build scala from source
    
    Reviewers: #triage_team, DataDrake
    
    Reviewed By: #triage_team, DataDrake
    
    Subscribers: DataDrake
    
    Maniphest Tasks: T6658
    
    Differential Revision: https://dev.getsol.us/D8996

--- packages/s/sbt/files/sbt.sh
+++ packages/s/sbt/files/sbt.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 if [[ -z "${SBT_OPTS}" ]]; then
   SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled"
 fi
-/usr/lib64/openjdk-8/bin/java $SBT_OPTS -jar /usr/share/sbt/sbt.jar "$@"
+
+if [ -z "$JAVA_HOME" ]; then
+    export JAVA_HOME=/usr/lib64/openjdk-11
+fi
+
+exec $JAVA_HOME/bin/java $SBT_OPTS -jar /usr/share/sbt/sbt.jar "$@"
