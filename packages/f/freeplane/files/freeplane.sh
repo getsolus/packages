bf0632864e9570244fd7279ef90be1d8eb0d8280
Campbell Jones <git@serebit.com>
2020-06-13 18:36:45 -0400

    freeplane: Update freeplane to 1.8.5
    Summary:
    Additionally, use JDK11 for build and runtime.
    
    Notable changes:
    - "Bug fixes" x8
    - Use twemoji icons along with replacing PNG icons with SVGs
    
    Full changelog is available [here](https://www.freeplane.org/info/history/history_en.txt).
    
    Test Plan: Mess around with the tutorial map
    
    Reviewers: #triage_team, DataDrake
    
    Reviewed By: #triage_team, DataDrake
    
    Subscribers: DataDrake
    
    Maniphest Tasks: T6658
    
    Differential Revision: https://dev.getsol.us/D9017

--- packages/f/freeplane/files/freeplane.sh
+++ packages/f/freeplane/files/freeplane.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-JAVACMD=/usr/lib64/openjdk-8/bin/java exec /usr/share/freeplane/freeplane.sh
+if [ -z "$JAVA_HOME" ]; then
+    export JAVA_HOME=/usr/lib64/openjdk-11
+fi
+
+if [ -z "$JAVACMD" ]; then
+    export JAVACMD=$JAVA_HOME/bin/java
+fi
+
+exec /usr/share/freeplane/freeplane.sh
