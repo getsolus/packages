4d18e678788f15ed7f7f9dd71c17b050e6953d58
Albert Krenz <albert.krenz@mailbox.org>
2020-07-14 13:52:36 +0300

    pdfsam: Update pdfsam to 4.1.3
    Summary:
    Update to version 4.1.3. Fixes T8804
    
    **Changelog:**
    
    - 4.1.3
      - Improved move up in selection table to show the above row when moving rows up
    - 4.1.2
     - Fixed few issues with AcroForms merging
     - Fixed few minor issues
    - 4.1.1
      - Prefix fields now save and restore the latest used value
      - Fixed split tasks to handle a case with PDF not conforming the PDF spec
      - Added a notification in case one of the input PDF files gets deleted before the task is executed
      - Fixed the bookmarks handling in case of input PDF file not conforming the PDF spec created with wkhtmltopdf
      - Fixed handling of a non breaking white space 0xA0 when found in bookmarks and the [BOOKMARK_NAME] is used as prefix
    - 4.0.4
     - Drag and drop of directories now asks the user if he wants to also add PDFs in subdirectories
     - Numerical sort of file names starting with digits
    - 4.0.3
     - Added a button to clear settings in Split modules
     - Added a button entry to load a list of files from a csv/text file
     - Added a setting to set Compression disabled by default
     - Added the [TOTAL_FILESNUMBER] placeholder
    - 4.0.1
     - Added a -verbose argument to get more information from the application
     - Fixed system property name to disable Settings panel to org.pdfsam.disable.settings.panel
     - When saving files password in the workspace file they are now encrypted and not plain text anymore
     - Salve and Load workspace both remember the latest used filename and they use it as predefined/suggested filename
    - 4.0.0
     - Added a way to clear recent workspaces
     - Added a way to hide Settings panel (Use case is an admin in a corporate env who doesn't want his users to be able to change the settings)
     - Smarter destination for Save Workspace
    
    **Packager Notes:**
    
    - Added myself (akrenz) as Maintainer.
    
    Test Plan: Merged two pdf files, extracting pages from a pdf.
    
    Reviewers: #triage_team, JoshStrobl
    
    Reviewed By: #triage_team, JoshStrobl
    
    Subscribers: quysuphuho, serebit, JoshStrobl
    
    Maniphest Tasks: T8804
    
    Differential Revision: https://dev.getsol.us/D9198

--- packages/p/pdfsam/files/pdfsam.sh
+++ packages/p/pdfsam/files/pdfsam.sh
@@ -4,4 +4,4 @@ if [ -z "$JAVA_HOME" ]; then
     export JAVA_HOME=/usr/lib64/openjdk-11
 fi
 
-exec $JAVA_HOME/bin/java -jar /usr/share/pdfsam/pdfsam.jar "$@"
+exec $JAVA_HOME/bin/java -jar /usr/share/pdfsam/pdfsam.jar -Xmx512M -Dapp.name="pdfsam-basic" -Dapp.pid="$$" -Dapp.home=/usr/share/pdfsam -Dbasedir=/usr/share/pdfsam -Dprism.lcdtetxt=false  "$@"
