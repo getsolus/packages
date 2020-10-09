289996cb54f0a891f371f728f63d0b7fb7e5e10d
chax <mcakaric@gmail.com>
2020-10-09 10:53:48 +0300

    avr-binutils: Fix avr-binutils: don't move lib
    Summary:
    This fixes problems for ArduinoIDE (T9300)
    Also removes need to specify LD_LIBRARY_PATH environment variable globally which is bad.
    
    Test Plan:
    Tried compiling simple Arduino sketch using ArduinoIDE
    Tried compiling qmk firmware for AVR board
    
    Reviewers: #triage_team, JoshStrobl
    
    Reviewed By: #triage_team, JoshStrobl
    
    Subscribers: JoshStrobl
    
    Maniphest Tasks: T9300
    
    Differential Revision: https://dev.getsol.us/D9803

--- packages/a/avr-binutils/files/50-avr-toolchain-path.sh
+++ packages/a/avr-binutils/files/50-avr-toolchain-path.sh
@@ -2,6 +2,5 @@
 
 export AVR_TOOLCHAIN_ROOT="/usr/share/avr"
 export PATH="$PATH:$AVR_TOOLCHAIN_ROOT/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$AVR_TOOLCHAIN_ROOT/lib"
 
 # End /usr/share/defaults/etc/profile.d/50-avr-toolchain-path.sh
