57695c633ef1385732a38b46396bede7f91adb0a
Campbell Jones <git@serebit.com>
2021-02-05 09:13:44 +0200

    llvm: Modify LLVM update script to take version as a parameter
    Summary: No package script changes, only update script changes.
    
    Test Plan: Generate LLVM 11.0.1 sources
    
    Reviewers: #triage_team, JoshStrobl
    
    Reviewed By: #triage_team, JoshStrobl
    
    Subscribers: JoshStrobl
    
    Differential Revision: https://dev.getsol.us/D10482

--- packages/l/llvm/update_sources.sh
+++ packages/l/llvm/update_sources.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-LLVMVERSION=10.0.1
-LINKS="https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVMVERSION}/llvm-${LLVMVERSION}.src.tar.xz
-    https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVMVERSION}/clang-${LLVMVERSION}.src.tar.xz
-    https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVMVERSION}/compiler-rt-${LLVMVERSION}.src.tar.xz
-    https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVMVERSION}/lld-${LLVMVERSION}.src.tar.xz
-    https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVMVERSION}/clang-tools-extra-${LLVMVERSION}.src.tar.xz
-    https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVMVERSION}/libcxx-${LLVMVERSION}.src.tar.xz
-    https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVMVERSION}/libcxxabi-${LLVMVERSION}.src.tar.xz
-    https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVMVERSION}/openmp-${LLVMVERSION}.src.tar.xz
-"
+
+LLVMVERSION="$1"
+
+if [[ -z $LLVMVERSION ]]; then
+    echo "Usage: $0 <version number>"
+    exit 1
+fi
+
+BASEURI="https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVMVERSION}"
+
+LINKS="$BASEURI/llvm-${LLVMVERSION}.src.tar.xz
+    $BASEURI/clang-${LLVMVERSION}.src.tar.xz
+    $BASEURI/compiler-rt-${LLVMVERSION}.src.tar.xz
+    $BASEURI/lld-${LLVMVERSION}.src.tar.xz
+    $BASEURI/clang-tools-extra-${LLVMVERSION}.src.tar.xz
+    $BASEURI/libcxx-${LLVMVERSION}.src.tar.xz
+    $BASEURI/libcxxabi-${LLVMVERSION}.src.tar.xz
+    $BASEURI/openmp-${LLVMVERSION}.src.tar.xz"
+
 pushd /tmp
-wget ${LINKS} > /dev/null
+wget -nv ${LINKS} > /dev/null
 for i in ${LINKS}; do
     j=`basename ${i}`
     echo "    - ${i} : `sha256sum ${j} | awk '{ print $1 }'`"
