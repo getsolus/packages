6b5ad8d99d858cce6c0596f7e336c0a5f2f3ae13
Longwu Ou <xulongwu4@gmail.com>
2022-01-14 14:45:52 -0500

    bash: Add $HOME/.local/bin to PATH by default
    Summary: Add $HOME/.local/bin to PATH by default
    
    Test Plan: Installed the package and verified $HOME/.local/bin in part of the PATH environment variable
    
    Reviewers: #triage_team, DataDrake
    
    Reviewed By: #triage_team, DataDrake
    
    Subscribers: DataDrake, Staudey, Harvey, livingsilver94
    
    Differential Revision: https://dev.getsol.us/D12522

--- packages/b/bash/files/profile/10-path.sh
+++ packages/b/bash/files/profile/10-path.sh
@@ -13,4 +13,8 @@ if [ -d "$HOME/bin" ]; then
   export PATH="$HOME/bin:$PATH"
 fi
 
+if [ -d "$HOME/.local/bin" ]; then
+  export PATH="$HOME/.local/bin:$PATH"
+fi
+
 # End /usr/share/defaults/etc/profile.d/10-path.sh
