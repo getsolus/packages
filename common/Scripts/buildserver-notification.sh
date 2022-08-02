f05b826694e44e4c7384460e0e129fed5f2ec5d9
Joey Riches <josephriches@gmail.com>
2022-08-02 14:28:01 +0100

    common: buildserver-notification: Use curl's If-Modified-Since option to save bandwidth
    The buildpage size is miniscule but savings are savings.
    
    Also increase the time to index timeout to 1m:30s due to paraview
    reaching the timeout limit.

--- common/Scripts/buildserver-notification.sh
+++ common/Scripts/buildserver-notification.sh
@@ -14,30 +14,37 @@ done
 
 if [[ ! -z "${DISABLE_BUILD_SUCCESS_NOTIFY}" ]]; then
     # For rebuild-template-script.sh
-    echo "Notification and sound ping for build success disabled due to DISABLE_SUCCESS_NOTIFY being set."
+    echo "Notification and sound ping for build success disabled due to DISABLE_BUILD_SUCCESS_NOTIFY being set."
 fi
 
 TAG=$(.././common/Scripts/gettag.py package.yml)
 BUILDSERVER_URL="https://build.getsol.us"
+BUILDPAGE="/tmp/buildpage.html"
+
+# Download the page
+curl -s $BUILDSERVER_URL -o $BUILDPAGE
 
 # Check it's actually been published first.
-if [[ ! $(curl -s $BUILDSERVER_URL | grep "${TAG}") ]] ; then
+if [[ ! $(grep "${TAG}" "${BUILDPAGE}" ) ]] ; then
     echo "${TAG} not found on build queue, has it been published?"
     exit 1
 fi
 
 # Get the latest build-id for the $TAG
-BUILDID=$(curl -s $BUILDSERVER_URL | grep -B 1 "${TAG}" | grep -o '[0-9]*' | sort -nr | head -1)
+BUILDID=$(grep -B 1 "${TAG}" "${BUILDPAGE}" | grep -o '[0-9]*' | sort -nr | head -1)
 echo "Build ID: ${BUILDID} | Tag: ${TAG}"
 
 # Look for build-ok from the build id
-while [[ ! $(curl -s $BUILDSERVER_URL | grep -A 4 "${BUILDID}" | grep build-ok) ]] ; do
+while [[ ! $(grep -A 4 "${BUILDID}" "${BUILDPAGE}" | grep build-ok) ]] ; do
 
     # Don't DoS the server
     sleep 20
 
+    # Download new build page if-modified-since (-z)
+    curl -s -z $BUILDPAGE $BUILDSERVER_URL -o $BUILDPAGE
+
     # Check if the build has failed
-    if [[ $(curl -s $BUILDSERVER_URL | grep -A 4 "${BUILDID}" | grep build-failed) ]] ; then
+    if [[ $(grep -A 4 "${BUILDID}" "${BUILDPAGE}" | grep build-failed) ]] ; then
         echo "Failed on the build server!"
         notify-send -u critical "${TAG} failed on the build server!" -t 0
         paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
@@ -46,6 +53,7 @@ while [[ ! $(curl -s $BUILDSERVER_URL | grep -A 4 "${BUILDID}" | grep build-ok) 
 done
 
 echo "Build succeeded on the buildserver! Waiting for it to be indexed..."
+rm ${BUILDPAGE}
 
 ### Now that it's built ensure we find it indexed into the repo.
 
@@ -77,13 +85,12 @@ update_index_if_changed() {
     fi
 }
 
-i=0
 # Look for the tag in the index
 # FIXME: some packages like libreoffice do not output a eopkg file matching the tag
 while [[ $(grep ${TAG} < /tmp/unstable-index.xml | wc -l) -lt 1 ]] ; do
-    ((i=i+1))
-    # Wait up to a minute (60 / (sleep) 5 = 12) before bailing
-    if [[ $i == 12 ]]; then
+    var=$((var+1))
+    # Wait up to 1m:30s (90 / (sleep) 5 = 18) before bailing
+    if [[ $var == 18 ]]; then
         echo "Successfully built but hasn't been found in the index yet, please manually check."
         notify-send -u low "${TAG} successfully built but hasen't been found in the index yet, please manually check." -t 0
         paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
