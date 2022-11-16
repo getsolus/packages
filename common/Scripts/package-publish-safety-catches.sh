e58a73ff5dd461721ba4fe7b77bcd3409a3c4033
Joey Riches <josephriches@gmail.com>
2022-11-16 10:57:06 +0000

    common: Scripts: Add primitive check that release has been bumped

--- common/Scripts/package-publish-safety-catches.sh
+++ common/Scripts/package-publish-safety-catches.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 
-# Safety catch to prevent system.base/system.devel pkgs where there are additions to abi_used_libs from being published accidently.
-# Otherwise, if a system.base/system.devel pkg depends on another pkg that isn't in system.base/system.devel the solbuild image
-# will refuse to update.
+# Safety catches when publishing packages. Not foolproof and there are edge cases where we don't want it which allows for force-pushing.
+# Currently:
+    # - Checks that the release has been bumped.
+    # - Warns if there are additions to abi_used_libs for system.{base,devel} packages.
 
-# FIXME: Check for rundeps changes as well
+# FIXME: Check for rundeps changes as well for system.{base,devel} packages.
+# FIXME: For the initial inclusion check that the release == 1
 
 LAST_COMMIT_DIFF=`git diff @~ @`
+
+PKG_BUMP=`git diff @~ @ package.yml | grep -w +release`
+
+# Check the release has been bumped
+if [[ $PKG_BUMP == "" ]]; then
+    echo "Warning: Cannot determine that the release has been bumped"
+    read -p "Press y to force-through. If unsure press any other key to abort." prompt
+    if [[ $prompt = "y" ]]; then
+        exit 0
+    else
+        exit 1
+    fi
+fi
+
+# Checks for additions to abi_used_libs for system.{base,devel} packages.
 
 if [[ `git grep -E 'system.base|system.devel' pspec_x86_64.xml` ]]; then
     SYSTEM_BASE_DEVEL_PKG=1
