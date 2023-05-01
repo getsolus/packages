0db8a7f5941dbbe3354d830f9ae24629fd8e513c
Rune Morling <ermo@serpentos.com>
2023-05-01 23:32:31 +0200

    vim: Add default editor, fix broken vi symlink
    T10513: Ensure EDITOR and VISUAL are set should a user remove the default editor nano.
    
    User changes in ~/.bashrc and its equivalent override this.
    
    Fix broken /usr/bin/vi -> /usr/bin/vim symlink.
    
    Signed-off-by: Rune Morling <ermo@serpentos.com>

---
+++ packages/v/vim/files/80-vim-is-default-EDITOR-and-VISUAL.fish
@@ -0,0 +1,4 @@
+# nano is installed out of the box on solus and is our default editor.
+set -gx EDITOR /usr/bin/vim
+set -gx VISUAL $EDITOR
+
---
+++ packages/v/vim/files/80-vim-is-default-EDITOR-and-VISUAL.sh
@@ -0,0 +1,4 @@
+# nano is installed out of the box on solus and is our default editor.
+export EDITOR="/usr/bin/vim"
+export VISUAL="${EDITOR}"
+
