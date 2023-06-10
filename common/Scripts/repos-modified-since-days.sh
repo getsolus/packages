1e954197bc2bb4149a671d852bd3da5dd19dacc0
Rune Morling <ermo@serpentos.com>
2023-06-10 13:27:32 +0200

    common: repos-modified-since-days: Clean up output + git cmd
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- common/Scripts/repos-modified-since-days.sh
+++ common/Scripts/repos-modified-since-days.sh
@@ -6,25 +6,26 @@
 # IFF an argument is supplied, it is expected to be a positive integer.
 # IFF an argument is NOT supplied (or 0 is supplied), the default value will be used.
 
+AFFECTED=()
 ARGS="$1"
 DAYS=
-AFFECTED=()
+DETECTCHANGES=""
 # Define it here because bash doesn't like quotes in pattern matching expressions ('=~' below)
 NUMERIC='(^[[:digit:]]+$)'
 
 function parse-cmd-line-args () {
     if [[ ${ARGS} == "" ]]; then
-        echo -en "No argument specified, "
+        echo -en "# No argument specified, "
         DAYS=7
     elif [[ ${ARGS} =~ $NUMERIC ]]; then
-        echo -en "Argument specified (${BASH_REMATCH[1]}), "
+        echo -en "# Argument specified (${BASH_REMATCH[1]}), "
         DAYS=${BASH_REMATCH[1]}
         # 0 is not a useful value in the "since 0 day(s)" sense
         if [[ $DAYS == 0 ]]; then
             DAYS=7
         fi
     else
-        echo -en "Nonsense argument specified (${ARGS}), "
+        echo -en "# Nonsense argument specified (${ARGS}), "
         DAYS=7
     fi
 }
@@ -41,11 +42,11 @@ function find-affected () {
     local output
     for i in $(find . -maxdepth 2 -name .git); do
         nom="$(dirname ${i})"
-        output="$(git -c color.ui=always -C "$nom" log -1 --pretty=format:"%C(yellow)%h%Creset %ad | %Cgreen%s%Creset %Cred%d%Creset %Cblue[%an]%Creset" --date=short  --after=@{${DAYS}.days.ago})"
-        if [[ -z "${outp}" ]]; then
+        output="$(git -C ${nom} log -1 --date=short --after=@{$DAYS.days.ago})"
+        if [[ -z "${output}" ]]; then
             continue
         fi
-        AFFECTED+="${nom}"
+        AFFECTED+="${nom} "
     done
     unset nom
     unset output
@@ -55,8 +56,9 @@ function find-affected () {
 check-dir
 parse-cmd-line-args
 echo -en "using $DAYS.\n"
-
+echo -e "#\n# change-detection command: git -C \$nom log -1 --date=short --after=@{$DAYS.days.ago}\n#"
 find-affected
-echo -e "Packages affected in the last ${DAYS} days:\n"
-echo -e "${AFFECTED[@]}"|sort|uniq
+
+echo "# Packages affected in the last ${DAYS} days:"
+echo "${AFFECTED[@]}"|tr ' ' '\n'|sort|uniq
 
