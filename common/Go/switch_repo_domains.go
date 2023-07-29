2efe1793f694490e662387ff3e9d6e9365769ce2
Joey Riches <josephriches@gmail.com>
2023-07-29 15:07:03 +0100

    common: Go/switch_repo_domain: Cleanup massive if indention & misc improvements
    - Use err != nil pattern to avoid massive if statment indendation.
    - Check if a directory isn't a git repo and skip if so.
    - Print how many repos where successfully switched.

--- common/Go/switch_repo_domains.go
+++ common/Go/switch_repo_domains.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -42,75 +43,90 @@ func main() {
 	PausePrompt()
 
 	// If we're continuing, we're in the right directory
-	if workDir, getWdErr := os.Getwd(); getWdErr == nil {
-		failedRepoChanges := []string{}
-		currentUser, getUserErr := user.Current() // Get the current user
-
-		if getUserErr != nil { // If we failed to get the current user
-			fmt.Println("Failed to determine the current user. Exiting.")
-			os.Exit(1)
-		}
-
-		workDir = strings.Replace(workDir, "~", currentUser.HomeDir, -1)
-		fmt.Printf("Determined full working directory path to be: %s\n", workDir)
-
-		dir, dirOpenErr := os.Open(workDir) // Attempt to open this directory
-
-		if dirOpenErr != nil {
-			fmt.Printf("Failed to open this directory for reading. Exiting with %s\n", dirOpenErr.Error())
-			os.Exit(1)
-		}
-
-		dirNames, dirReadErr := dir.Readdirnames(-1) // Get all the directory names
-
-		if dirReadErr != nil {
-			fmt.Printf("Failed to read the contents of this directory. Exiting with %s\n", dirReadErr.Error())
-		}
-
-		for _, dir := range dirNames { // For each directory
-			fmt.Printf("Attempting to update %s\n", dir)
-			repoPath := filepath.Join(workDir, dir)
-			repoGitConfigPath := filepath.Join(repoPath, ".git", "config") // Combine the paths to get to .git/config
-
-			repoFile, repoOpenErr := os.Open(repoPath) // Read the path so we can determine if it is a directory or file
-
-			if repoOpenErr == nil { // Did not fail to read
-				repoStats, repoStatErr := repoFile.Stat() // Get the stats
-
-				if repoStatErr == nil {
-					if repoStats.IsDir() { // Is a directory
-						fmt.Printf("Attempting to read %s\n", repoGitConfigPath)
-						configContents, configReadErr := ioutil.ReadFile(repoGitConfigPath)
-
-						if configReadErr == nil {
-							config := string(configContents[:])                                                              // Convert to a string
-							config = strings.Replace(config, "http://dev.getsol.us/", "https://dev.getsol.us/", -1)  // Replace HTTP with HTTPS
-							config = strings.Replace(config, "https://dev.getsol.us/", "https://dev.getsol.us/", -1) // Replace HTTPS
-							config = strings.Replace(config, "ssh://vcs@dev.getsol.us:2222/", "git@github.com:solus-packages/nano.git", -1)
-
-							writeErr := ioutil.WriteFile(repoGitConfigPath, []byte(config), 0644)
-
-							if writeErr != nil {
-								fmt.Println("Failed to write the config. Adding to a report at the end.")
-								failedRepoChanges = append(failedRepoChanges, dir) // Add this directory to our failed repo changes
-							}
-						} else {
-							fmt.Println("Failed to read the file. Adding to a report to show at the end.")
-							failedRepoChanges = append(failedRepoChanges, dir) // Add this directory to our failed repo changes
-						}
-					}
-				}
-			}
-		}
-
-		if len(failedRepoChanges) > 0 { // If we failed to update some repos
-			fmt.Println("List of failed repo updates:")
-			for _, repo := range failedRepoChanges {
-				fmt.Println(repo)
-			}
-		}
-	} else {
+	workDir, getWdErr := os.Getwd();
+	if getWdErr != nil {
 		fmt.Println("Failed to determine the current working directory. Exiting.")
 		os.Exit(1)
+	}
+
+	failedRepoChanges := []string{}
+	currentUser, getUserErr := user.Current() // Get the current user
+
+	if getUserErr != nil { // If we failed to get the current user
+		fmt.Println("Failed to determine the current user. Exiting.")
+		os.Exit(1)
+	}
+
+	workDir = strings.Replace(workDir, "~", currentUser.HomeDir, -1)
+	fmt.Printf("Determined full working directory path to be: %s\n", workDir)
+
+	dir, dirOpenErr := os.Open(workDir) // Attempt to open this directory
+	if dirOpenErr != nil {
+		fmt.Printf("Failed to open this directory for reading. Exiting with %s\n", dirOpenErr.Error())
+		os.Exit(1)
+	}
+
+	dirNames, dirReadErr := dir.Readdirnames(-1) // Get all the directory names
+	if dirReadErr != nil {
+		fmt.Printf("Failed to read the contents of this directory. Exiting with %s\n", dirReadErr.Error())
+	}
+
+	counter := 0
+	for _, dir := range dirNames { // For each directory
+
+		repoPath := filepath.Join(workDir, dir)
+		repoGitConfigPath := filepath.Join(repoPath, ".git", "config") // Combine the paths to get to .git/config
+
+		repoFile, repoOpenErr := os.Open(repoPath) // Read the path so we can determine if it is a directory or file
+		if repoOpenErr != nil {
+			fmt.Printf("Failed to open %s", repoPath)
+			failedRepoChanges = append(failedRepoChanges, dir)
+			continue
+		}
+
+		repoStats, repoStatErr := repoFile.Stat() // Get the stats
+		if repoStatErr != nil {
+			fmt.Printf("Failed to stat %s. Adding to a report at the end.", repoFile.Name())
+			failedRepoChanges = append(failedRepoChanges, dir)
+			continue
+		}
+		if !repoStats.IsDir() {
+			fmt.Printf("%s isn't a directory, skipping.\n", repoStats.Name())
+			continue
+		}
+
+		if _, err := os.Stat(repoGitConfigPath); errors.Is(err, os.ErrNotExist) {
+			fmt.Printf("%s isn't a git repository, skipping.\n", repoGitConfigPath)
+			continue
+		}
+
+		fmt.Printf("Attempting to read: %s\n", repoGitConfigPath)
+		configContents, configReadErr := os.ReadFile(repoGitConfigPath)
+		if configReadErr != nil {
+			fmt.Printf("Failed to read %s. Adding to a report at the end.\n", repoGitConfigPath)
+			failedRepoChanges = append(failedRepoChanges, dir)
+			continue
+		}
+
+		fmt.Printf("\033[35mAttempting to update: %s\033[0m\n", dir)
+		config := string(configContents[:])                                                                  // Convert to a string
+		config = strings.Replace(config, "http://dev.getsol.us/", "https://github.com/solus-packages/", -1)  // Replace HTTP with HTTPS
+		config = strings.Replace(config, "https://dev.getsol.us/", "https://github.com/solus-packages/", -1) // Replace HTTPS
+		config = strings.Replace(config, "ssh://vcs@dev.getsol.us:2222/", "git@github.com:solus-packages/", -1)
+		writeErr := ioutil.WriteFile(repoGitConfigPath, []byte(config), 0644)
+		if writeErr != nil {
+			fmt.Println("Failed to write the config. Adding to a report at the end.")
+			failedRepoChanges = append(failedRepoChanges, dir)
+		}
+		counter += 1
+	}
+
+	if len(failedRepoChanges) > 0 { // If we failed to update some repos
+		fmt.Println("List of failed repo updates:")
+		for _, repo := range failedRepoChanges {
+			fmt.Println(repo)
+		}
+	} else {
+		fmt.Printf("\033[32mSuccessfully updated %d repos\033[0m\n", counter)
 	}
 }
