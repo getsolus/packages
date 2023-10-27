999ebf634c7e8c0c81936e52e05e0a149bb4b383
Rune Morling <ermo@serpentos.com>
2023-10-27 15:11:24 +0200

    baselayout: Update iana protocols and services
    **Summary**
    
    Our /etc/protocols and /etc/services files (known ports and known
    services) were very outdated.
    
    This commit fixes that and updates the script we use to also check
    sha256sum of the downloaded files.
    
    Resolves #340
    
    **Test plan**
    
    Install the updated baselayout, run `sudo ss -platu`, check that
    services and protocols are listed correctly.
    
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- packages/b/baselayout/files/update-iana.sh
+++ packages/b/baselayout/files/update-iana.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-IANA_VERSION=2.30
+#
+# setup phase
+#
+IANA_VERSION=20231019
 
-wget http://sethwklein.net/iana-etc-$IANA_VERSION.tar.bz2
-tar xvf iana-etc-$IANA_VERSION.tar.bz2
+echo "WORKDIR is ${PWD}."
+wget https://github.com/Mic92/iana-etc/releases/download/${IANA_VERSION}/iana-etc-${IANA_VERSION}.tar.gz
+wget https://github.com/Mic92/iana-etc/releases/download/${IANA_VERSION}/iana-etc-${IANA_VERSION}.tar.gz.sha256
 
+# Ensure that we have a good download
+EXPECTED_SHA256SUM="$(cat iana-etc-${IANA_VERSION}.tar.gz.sha256)"
+ACTUAL_SHA256SUM="$(sha256sum iana-etc-20231019.tar.gz| cut -d ' ' -f1)"
+if [[ ! "${EXPECTED_SHA256SUM}" == "${ACTUAL_SHA256SUM}" ]]; then
+    echo "sha256sum mismatch for iana-etc-${IANA_VERSION}.tar.gz.sha256, exiting!" && exit 1
+fi
+unset EXPECTED_SHA256SUM
+unset ACTUAL_SHA256SUM
+
+tar -xvf iana-etc-${IANA_VERSION}.tar.gz
 pushd iana-etc-$IANA_VERSION
 
-sed -i 's:file=protocol-numbers:file=protocol-numbers/protocol-numbers.txt:' Makefile
-
-make get
-LC_ALL=C make
-make test
-
+#
+# install phase
+#
 cp {services,protocols} ../
-
 popd
 
-# Cleanup
-rm -rf iana-etc-$IANA_VERSION
+#
+# cleanup phase
+#
+rm -rvf iana-etc-$IANA_VERSION*
