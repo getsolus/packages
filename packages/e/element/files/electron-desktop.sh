68602cbde52931340f5bf7c771be92839096c693
Reilly Brogan <reilly@reillybrogan.com>
2023-11-09 16:51:49 -0600

    element: Use Wayland by default
    Signed-off-by: Reilly Brogan <reilly@reillybrogan.com>

---
+++ packages/e/element/files/electron-desktop.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+# Launches element-desktop with flags specified in $XDG_CONFIG_HOME/element-desktop-flags.conf
+
+# Make script fail if `cat` fails for some reason
+set -e
+
+# Set default value if variable is unset/null
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+
+# Attempt to read a config file if it exists
+if [ -r "${XDG_CONFIG_HOME}/element-desktop-flags.conf" ]; then
+  ELEMENT_DESKTOP_FLAGS="$(cat "$XDG_CONFIG_HOME/element-desktop-flags.conf")"
+fi
+
+# Wayland auto-detection. If the session is a wayland session then this will launch as a Wayland window unless the ELEMENT_NO_WAYLAND variable is set
+if [ -z "${ELEMENT_NO_WAYLAND+set}" ]; then
+  ELEMENT_DESKTOP_FLAGS="$ELEMENT_DESKTOP_FLAGS --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations,WebRTCPipeWireCapturer"
+fi
+
+# shellcheck disable=SC2086
+exec /usr/share/element/element-desktop $ELEMENT_DESKTOP_FLAGS "$@"
