96bf8c5dcb3c192b3755b2ac679fba58bd76fc65
Thomas Staudinger <Staudi.Kaos@gmail.com>
2024-05-30 00:57:42 +0200

    common: Rework update_stateless.sh script for monorepo
    Also update STATELESSNESS file
    
    Signed-off-by: Thomas Staudinger <Staudi.Kaos@gmail.com>

--- common/Scripts/update_stateless.sh
+++ common/Scripts/update_stateless.sh
@@ -6,15 +6,19 @@ fi
 
 touch STATELESSNESS
 
-pushd ..
+pushd ../packages
 
-for f in `ls -1`; do
-    if [ -e "$f/pspec.xml" ]; then
-        grep -H \>/etc $f/pspec.xml >> common/STATELESSNESS
-    fi
-    if [ -e "$f/pspec_x86_64.xml" ]; then
-        grep -H \>/etc $f/pspec_x86_64.xml >> common/STATELESSNESS
-    fi
+for f in `ls`; do
+    pushd $f
+    for p in `ls`; do
+        if [ -e "$p/pspec.xml" ]; then
+            grep -H \>/etc $p/pspec.xml >> ../../common/STATELESSNESS
+        fi
+        if [ -e "$p/pspec_x86_64.xml" ]; then
+            grep -H \>/etc $p/pspec_x86_64.xml >> ../../common/STATELESSNESS
+        fi
+    done
+    popd
 done
 
 popd
