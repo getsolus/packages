25adff2be1f57a75f9ba3edd20fe09f6ab5dbdb3
Reilly Brogan <reilly@reillybrogan.com>
2024-06-26 18:16:56 -0500

    usysconf-epoch: Implement file orphaning
    Implement the functionality to orphan files in case something goes wrong and they can't be copied/moved. Use the file hash and a normalized path in the file name in order to prevent collisions.
    
    Signed-off-by: Reilly Brogan <reilly@reillybrogan.com>

--- packages/u/usysconf-epoch/files/epoch.sh
+++ packages/u/usysconf-epoch/files/epoch.sh
@@ -8,7 +8,7 @@ if [ -z "${I_UNDERSTAND_THAT_THIS_SCRIPT_CAN_BREAK_MY_SYSTEM+set}" ]; then
 fi
 
 # This is where orphaned files will be moved to
-ORPHAN_DIR="${ORPHAN_DIR:=/var/orphaned}"
+ORPHAN_DIR="${ORPHAN_DIR:=/var/usr-merge-orphaned-files}"
 
 # Manually specify the path of binaries needed since we're messing with /bin and /sbin
 CP="${CP:=/usr/bin/cp}"
@@ -16,6 +16,8 @@ DIRNAME="${DIRNAME:=/usr/bin/dirname}"
 FIND="${FIND:=/usr/bin/find}"
 MKDIR="${MKDIR:=/usr/bin/mkdir}"
 READLINK="${READLINK:=/usr/bin/readlink}"
+RM="${RM:=/usr/bin/rm}"
+SHA256SUM="${SHA256SUM:=/usr/bin/sha256sum}"
 STAT="${STAT:=/usr/bin/stat}"
 TOUCH="${TOUCH:=/usr/bin/touch}"
 
@@ -68,6 +70,34 @@ create_dir_components () {
     $MKDIR --mode="$dir_stat" $dir_name || true && action="orphan"
 }
 
+# This script takes a given file and moves it to the orphaned files directory
+# To reduce the risk of any errors occurring or file collisions we flatten the directory path and append the file hash
+orphan_file () {
+    if ! test -d "$ORPHAN_DIR"; then
+        $MKDIR "$ORPHAN_DIR"
+    fi
+
+    local file_to_orphan="$1"
+    local file_checksum=($($SHA256SUM "$file_to_orphan"))
+    local new_file_name="$ORPHAN_DIR/root${file_to_orphan//\//-}.$file_checksum"
+
+    # Check if the orphaned file already exists
+    if test -e "$new_file_name"; then
+        # It somehow exists, likely from a previous invocation of this script. Make sure it was a successful copy
+        local orphan_checksum=($($SHA256SUM "$new_file_name"))
+        if [[ "$file_checksum" == "$orphan_checksum" ]]; then
+            return 0
+        fi
+
+        # The orphaned file exists but does not have the correct checksum. Assume it's an incomplete copy
+        printf "Deleting previously orphaned file $new_file_name\n"
+        $RM -v "$new_file_name"
+    fi
+
+    printf "Copying file $file_to_orphan to $new_file_name\n"
+    $CP -av "$file_to_orphan" "$new_file_name"
+}
+
 merge_dir () {
     if needs_to_be_merged $1; then
         echo ""
@@ -86,7 +116,7 @@ merge_dir () {
             create_dir_components $($DIRNAME "$new_location")
 
             # This is where I'm stopped currently, we need to correctly handle the logic of moving files here
-
+            orphan_file "$old_location"
         done
     fi
 }
