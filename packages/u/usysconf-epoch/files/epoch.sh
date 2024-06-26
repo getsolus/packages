9c788582a06fff88c363067dc647d929ccbe50d2
Reilly Brogan <reilly@reillybrogan.com>
2024-06-26 15:51:58 -0500

    usysconf-epoch: Add early exit, systemd unit
    Signed-off-by: Reilly Brogan <reilly@reillybrogan.com>

--- packages/u/usysconf-epoch/files/epoch.sh
+++ packages/u/usysconf-epoch/files/epoch.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/bash
 set -euo pipefail
 
+# Temp: Exit if I_UNDERSTAND_THAT_THIS_SCRIPT_CAN_BREAK_MY_SYSTEM is not set
+# This will be removed once we are ready to enable this by default
+if [ -z "${I_UNDERSTAND_THAT_THIS_SCRIPT_CAN_BREAK_MY_SYSTEM+set}" ]; then
+    exit 0
+fi
+
+# TODO place marker for eopkg to transition to new repo here
+
 printf "I'm a little teapot"
