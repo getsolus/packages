1e2a47be0664f183bbc4c03982df18ab504b7d46
Reilly Brogan <reilly@reillybrogan.com>
2024-11-13 21:04:24 -0600

    bitwarden-desktop: Update to v2024.11.0
    Signed-off-by: Reilly Brogan <reilly@reillybrogan.com>

--- packages/b/bitwarden-desktop/files/bitwarden.sh
+++ packages/b/bitwarden-desktop/files/bitwarden.sh
@@ -14,7 +14,12 @@ fi
 
 # Wayland auto-detection. If the session is a wayland session then this will launch as a Wayland window unless the BITWARDEN_NO_WAYLAND variable is set
 if [ -z "${BITWARDEN_NO_WAYLAND+set}" ]; then
-  BITWARDEN_FLAGS="$BITWARDEN_FLAGS --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations"
+  if [ -z "${ELECTRON_OZONE_PLATFORM_HINT+set}" ]; then
+    export ELECTRON_OZONE_PLATFORM_HINT="auto"
+  fi
 fi
+
+# disable core dumps (matches upstream wrapper script)
+ulimit -c 0
 
 exec /usr/share/bitwarden-desktop/bitwarden-desktop $BITWARDEN_FLAGS $@
