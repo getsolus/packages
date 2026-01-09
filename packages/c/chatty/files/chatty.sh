6f086059137d5def66735191daa594ef6a278b15
Jakob Gezelius <jakob@knugen.nu>
2026-01-09 19:29:00 +0100

    chatty: Update to v0.28
    **Summary**
    - Release notes can be found [here](https://github.com/chatty/chatty/releases/tag/v0.28).

--- packages/c/chatty/files/chatty.sh
+++ packages/c/chatty/files/chatty.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$JAVA_HOME" ]; then
-    export JAVA_HOME=/usr/lib64/openjdk-17
+    export JAVA_HOME=/usr/lib64/openjdk-21
 fi
 
 exec $JAVA_HOME/bin/java -jar /usr/share/chatty/Chatty.jar "$@"
