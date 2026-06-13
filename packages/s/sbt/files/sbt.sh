1680f94045c8b1f7e59900c856d8b7592fb0f582
Jared Cervantes <jared@jaredcervantes.com>
2026-06-13 09:06:34 -0400

    sbt: Update to v1.12.11
    **Summary**
    - Release notes can be found [here](https://github.com/sbt/sbt/releases).
    - Build with OpenJDK-25
    
    Signed-off-by: Jared Cervantes <jared@jaredcervantes.com>

--- packages/s/sbt/files/sbt.sh
+++ packages/s/sbt/files/sbt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ -z "${SBT_OPTS}" ]]; then
-  SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled"
+  SBT_OPTS="-Xms512M -Xmx1536M -Xss1M"
 fi
 
 if [ -z "$JAVA_HOME" ]; then
-    export JAVA_HOME=/usr/lib64/openjdk-21
+    export JAVA_HOME=/usr/lib64/openjdk-25
 fi
 
 exec $JAVA_HOME/bin/java $SBT_OPTS -jar /usr/share/sbt/sbt.jar "$@"
